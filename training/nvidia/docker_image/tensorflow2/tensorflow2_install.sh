5f7415149fe4690c381a8d6f62090df579b9dc92
UP <UP@.wwlwenli@163.com>
2023-02-06 16:51:12 +0800

    Modify according to review comments

--- training/nvidia/docker_image/tensorflow2/tensorflow2_install.sh
+++ training/nvidia/docker_image/tensorflow2/tensorflow2_install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 # Currently we have something to do when building docker image for tensorflow2.
-pip install gin-config==0.1.1
-pip install tensorflow_model_optimization
-pip install absl-py==0.15.0
