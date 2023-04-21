61d0952118ac1cbdc5510e42c6d72176e341e42a
wangzhengdan <wangzhengdan@baidu.com>
2023-04-21 07:08:09 +0000

    fix ddp

--- training/benchmarks/vit/pytorch/train/trainer_adapter.py
+++ training/benchmarks/vit/pytorch/train/trainer_adapter.py
@@ -1,28 +1,20 @@
-import os
-from port_for import is_available
-import torch
-import torch.distributed as dist
-from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
 import config
-
-from torch import nn, Tensor
+from torch import nn
+import torch.distributed as dist
 from driver.dist_pytorch import main_proc_print
-from typing import Tuple
-from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.parallel import DistributedDataParallel as NativeDDP
+
+try:
+    from apex import amp
+    from apex.parallel import DistributedDataParallel as ApexDDP
+    from apex.parallel import convert_syncbn_model
+    has_apex = True
+except ImportError:
+    has_apex = False
 
 
 def convert_model(model: nn.Module) -> nn.Module:
     return model
-
-
-def create_optimizer(model, args):
-    optimizer = torch.optim.SGD(model.parameters(),
-                                lr=args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
-    return optimizer
-
 
 def model_to_fp16(model):
     # To prevent OOM for model sizes that cannot fit in GPU memory in full precision
@@ -31,20 +23,14 @@ def model_to_fp16(model):
         model.half()
     return model
 
-
-def model_to_ddp(model: nn.Module) -> nn.Module:
+def model_to_ddp(model: nn.Module, use_amp) -> nn.Module:
     if dist.is_available() and dist.is_initialized():
-        model = DDP(model, device_ids=[config.local_rank])
+        if has_apex and use_amp == 'apex':
+            # Apex DDP preferred unless native amp is activated
+            main_proc_print("Using NVIDIA APEX DistributedDataParallel.")
+            model = ApexDDP(model, delay_allreduce=True)
+        else:
+            main_proc_print("Using native Torch DistributedDataParallel.")
+            model = NativeDDP(model)
+        # NOTE: EMA model does not need to be wrapped by DDP
     return model
-
-
-def create_grad_scaler():
-    return None
-
-
-def backward(step: int, loss: torch.Tensor, optimizer: Optimizer):
-    loss.backward()
-    update_step = step % config.gradient_accumulation_steps == 0
-    if update_step:
-        optimizer.step()
-        optimizer.zero_grad()
--- training/nvidia/vit-pytorch/extern/trainer_adapter.py
+++ training/nvidia/vit-pytorch/extern/trainer_adapter.py
@@ -1,6 +1,17 @@
 import config
 from torch import nn
+import torch.distributed as dist
 from driver.dist_pytorch import main_proc_print
+from torch.nn.parallel import DistributedDataParallel as NativeDDP
+
+try:
+    from apex import amp
+    from apex.parallel import DistributedDataParallel as ApexDDP
+    from apex.parallel import convert_syncbn_model
+    has_apex = True
+except ImportError:
+    has_apex = False
+
 
 def convert_model(model: nn.Module) -> nn.Module:
     return model
@@ -10,4 +21,16 @@ def model_to_fp16(model):
     if config.fp16:
         main_proc_print(" > use fp16...")
         model.half()
+    return model
+
+def model_to_ddp(model: nn.Module, use_amp) -> nn.Module:
+    if dist.is_available() and dist.is_initialized():
+        if has_apex and use_amp == 'apex':
+            # Apex DDP preferred unless native amp is activated
+            main_proc_print("Using NVIDIA APEX DistributedDataParallel.")
+            model = ApexDDP(model, delay_allreduce=True)
+        else:
+            main_proc_print("Using native Torch DistributedDataParallel.")
+            model = NativeDDP(model)
+        # NOTE: EMA model does not need to be wrapped by DDP
     return model
