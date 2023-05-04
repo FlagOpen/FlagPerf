6f9b86fee2e1f8a85a0c881ed7f2407bd690cece
zhouyu <zhouyu@baai.ac.cn>
2023-05-04 10:45:18 +0800

    resolve conflicts for nv configs

--- training/nvidia/resnet50-pytorch/config/config_A100x1x2.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x1x2.py
@@ -11,10 +11,4 @@ beta_1: float = 0.9
 beta_2: float = 0.99
 eps: float = 1e-08
 
-<<<<<<< HEAD
 seed = 23333
-training_event = None
-init_checkpoint = "checkpoint.70.pth.tar"
-=======
-seed = 23333
->>>>>>> 6abcadc223c5a301eea089b6783092b46ba2aa10
--- training/nvidia/resnet50-pytorch/config/config_A100x2x8.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x2x8.py
@@ -14,10 +14,4 @@ beta_2: float = 0.99
 eps: float = 1e-08
 
 seed = 23333
-<<<<<<< HEAD
-max_samples_termination = 43912600
-training_event = None
-init_checkpoint = "checkpoint.70.pth.tar"
-=======
 max_samples_termination = 4391260000
->>>>>>> 6abcadc223c5a301eea089b6783092b46ba2aa10
