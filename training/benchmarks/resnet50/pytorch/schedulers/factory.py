7d85e935c012247cf6f45ac5062e885878e23183
zhouyu <zhouyu@baai.ac.cn>
2023-05-05 11:02:46 +0800

    refine create_schedular. use args instead of hard-code

--- training/benchmarks/resnet50/pytorch/schedulers/factory.py
+++ training/benchmarks/resnet50/pytorch/schedulers/factory.py
@@ -2,7 +2,8 @@ from torch.optim.lr_scheduler import StepLR
 from typing import Any
 
 
-def create_scheduler(optimizer: Any) -> Any:
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
+def create_scheduler(optimizer: Any, args) -> Any:
+    scheduler = StepLR(optimizer,
+                       step_size=args.lr_step_size,
+                       gamma=args.lr_gamma)
     return scheduler
