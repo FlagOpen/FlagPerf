0a18c6836932f90f0d7f44dc0e04a1ef54f8c5db
zhouyu <zhouyu@baai.ac.cn>
2023-05-09 19:44:51 +0800

    add __init__.py

---
+++ training/benchmarks/tacotron2/pytorch/model/__init__.py
@@ -0,0 +1,11 @@
+from .models.model_helper import get_model_config, get_model
+
+
+def create_model(args):
+    model_config = get_model_config(args)
+    uniform_initialize_bn_weight = not args.disable_uniform_initialize_bn_weight
+    model = get_model(
+        model_config,
+        cpu_run=False,
+        uniform_initialize_bn_weight=uniform_initialize_bn_weight)
+    return model
