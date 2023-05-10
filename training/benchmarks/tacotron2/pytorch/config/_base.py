83a23428e2b9b72959744ab0b380b03a49da5a92
zhouyu <zhouyu@baai.ac.cn>
2023-05-10 15:01:45 +0800

    add encoder & decoder parameters for _base.py

--- training/benchmarks/tacotron2/pytorch/config/_base.py
+++ training/benchmarks/tacotron2/pytorch/config/_base.py
@@ -137,3 +137,40 @@ mel_fmax: float = 8000
 n_mel_channels: int = 80
 # Use mask padding
 mask_padding: bool = False
+"""Symbols parameters"""
+# Number of symbols in dictionary
+n_symbols: int = 0
+# Input embedding dimension
+symbols_embedding_dim: int = 512
+"""Encoder parameters"""
+# Encoder kernel size
+encoder_kernel_size: int = 5
+# Number of encoder convolutions
+encoder_n_convolutions: int = 3
+# Encoder embedding dimension
+encoder_embedding_dim: int = 512
+"""Decoder parameters"""
+# Number of frames processed per step
+n_frames_per_step: int = 1
+# Number of units in decoder LSTM
+decoder_rnn_dim: int = 1024
+# Number of ReLU units in prenet layers
+prenet_dim: int = 256
+# Maximum number of output mel spectrograms
+max_decoder_steps : int = 2000
+# Probability threshold for stop token
+gate_threshold:float = 0.5
+# Dropout probability for attention LSTM
+p_attention_dropout: float = 0.1
+# Dropout probability for decoder LSTM
+p_decoder_dropout:float = 0.1
+# Stop decoding once all samples are finished
+decoder_no_early_stopping:bool = False
+
+"""Mel-post processing network parameters"""
+# Postnet embedding dimension
+postnet_embedding_dim:int = 512
+# Postnet kernel size
+postnet_kernel_size : int = 5
+# Number of postnet convolutions
+postnet_n_convolutions:int = 5
