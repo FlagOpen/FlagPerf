fdc561bdd73b740bbf4d478e0c00a2a3e6ae50d3
zhouyu <zhouyu@baai.ac.cn>
2023-05-10 11:34:15 +0800

    update max_samples_termination for nv configs

--- training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
@@ -7,4 +7,4 @@ train_batch_size = 32
 eval_batch_size = train_batch_size
 max_steps = 100000000
 # total samples of 100 epoch for iterating ILSVRC2012 dataset 
-max_samples_termination = 113116700
+max_samples_termination = 133116700
