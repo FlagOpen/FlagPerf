9fce10f100215c853dd91f128222b3cd93659947
zhouyu <zhouyu@baai.ac.cn>
2023-05-10 10:23:11 +0800

    resnet50 torch:update nv configs

--- training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
@@ -5,6 +5,6 @@ seed = 23333
 learning_rate = 0.1
 train_batch_size = 32
 eval_batch_size = train_batch_size
-max_steps = 10000*10000
-max_samples_termination = 43912600*1000
-
+max_steps = 100000000
+# total samples of 100 epoch for iterating ILSVRC2012 dataset 
+max_samples_termination = 113116700
