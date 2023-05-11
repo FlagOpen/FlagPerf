07c77ca78dc1e3964a6de30acafcf305e8a22bfe
zhouyu <zhouyu@baai.ac.cn>
2023-05-11 17:19:19 +0800

    remove redudant configs in nv configs

--- training/nvidia/mask_rcnn-pytorch/config/config_A100x1x1.py
+++ training/nvidia/mask_rcnn-pytorch/config/config_A100x1x1.py
@@ -1,17 +1,12 @@
-vendor:str = "nvidia"
+vendor = "nvidia"
+dist_backend = "nccl"
+
 train_batch_size = 16
 eval_batch_size = 16
 
-
-dist_backend = "nccl"
-weight_decay = 1e-4
-adam_beta1 = 0.9
-adam_beta2 = 0.999
-adam_eps = 1e-08
 gradient_accumulation_steps = 1
 warmup = 0.1
 lr = 0.01
 log_freq = 1
 seed = 10483
 max_samples_termination = 5553080
-training_event = None
