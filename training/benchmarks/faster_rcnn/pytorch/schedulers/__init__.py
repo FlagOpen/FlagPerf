0e7833baaff0aefd35b4867b87429de8ecece679
shh2000 <13820618441@163.com>
2023-05-15 16:29:22 +0800

    fix name

--- training/benchmarks/faster_rcnn/pytorch/schedulers/__init__.py
+++ training/benchmarks/faster_rcnn/pytorch/schedulers/__init__.py
@@ -2,7 +2,7 @@ from torch.optim.lr_scheduler import MultiStepLR
 
 
 def create_scheduler(optimizer, args):
-    lr_sche = MultiStepLR(optimizer,
-                          milestones=args.lr_steps,
-                          gamma=args.lr_gamma)
-    return lr_sche
+    lr_scheduler = MultiStepLR(optimizer,
+                               milestones=args.lr_steps,
+                               gamma=args.lr_gamma)
+    return lr_scheduler
