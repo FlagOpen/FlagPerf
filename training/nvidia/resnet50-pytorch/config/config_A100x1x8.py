b01d8a72f8e7dea8702fcc35e8208eca4f001c6b
zhouyu <zhouyu@baai.ac.cn>
2023-05-15 17:34:40 +0800

    clean max_steps from nv configs

--- training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
@@ -5,6 +5,5 @@ seed = 23333
 learning_rate = 0.1
 train_batch_size = 32
 eval_batch_size = train_batch_size
-max_steps = 100000000
 # total samples of 100 epoch for iterating ILSVRC2012 dataset 
 max_samples_termination = 133116700
