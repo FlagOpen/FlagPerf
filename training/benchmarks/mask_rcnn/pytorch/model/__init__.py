ba34d6b7ae9cb15e7ad0327ed4ba4960a901d1e8
zhouyu <zhouyu@baai.ac.cn>
2023-05-17 13:13:10 +0800

    rename variables

--- training/benchmarks/mask_rcnn/pytorch/model/__init__.py
+++ training/benchmarks/mask_rcnn/pytorch/model/__init__.py
@@ -10,10 +10,10 @@ def create_model(args):
                                             args.pretrained_path)
     coco_pretrained_path = os.path.join(args.data_dir,
                                         args.coco_weights_pretrained_path)
-    url_resnet50 = "file://" + resnet50_pretrained_path
+    url_resnet50_backbone = "file://" + resnet50_pretrained_path
     url_coco_pretrained = "file://" + coco_pretrained_path
 
-    torchvision.models.resnet.__dict__['model_urls']['resnet50'] = url_resnet50
+    torchvision.models.resnet.__dict__['model_urls']['resnet50'] = url_resnet50_backbone
     torchvision.models.detection.mask_rcnn.__dict__['model_urls'][ 'maskrcnn_resnet50_fpn_coco'] = url_coco_pretrained
     model = maskrcnn_resnet50_fpn(pretrained=args.use_pretrained)
 
