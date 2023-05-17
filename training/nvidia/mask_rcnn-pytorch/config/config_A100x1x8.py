d6bbdbdfe0fc633a022643f97297125d10b7ae60
zhouyu <zhouyu@baai.ac.cn>
2023-05-17 17:58:11 +0800

    set bs=16, lr=0.12

--- training/nvidia/mask_rcnn-pytorch/config/config_A100x1x8.py
+++ training/nvidia/mask_rcnn-pytorch/config/config_A100x1x8.py
@@ -1,11 +1,11 @@
 vendor = "nvidia"
 dist_backend = "nccl"
 
-train_batch_size = 2
+train_batch_size = 16
 eval_batch_size = train_batch_size
 
 gradient_accumulation_steps = 1
 warmup = 0.1
-lr = 0.02
+lr = 0.12
 log_freq = 1
 seed = 10483
