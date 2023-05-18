aedd0ab945c9aa2c72f7d86b7f65e9c01aa5580a
Feilei Du <dufeilei@foxmail.com>
2023-05-18 02:33:16 +0000

    support cpm on xpu

--- training/kunlunxin/cpm-pytorch/config/config_R300x1x8.py
+++ training/kunlunxin/cpm-pytorch/config/config_R300x1x8.py
@@ -11,7 +11,8 @@ gradient_accumulation_steps = 1
 
 train_batch_size = 32
 eval_batch_size = train_batch_size
-max_steps = 10000
+max_steps = 4000
+max_samples_termination = 4391260
 
 warmup = 0.2
 learning_rate = 0.0005
@@ -19,13 +20,6 @@ learning_rate = 0.0005
 beta_1: float = 0.9
 beta_2: float = 0.99
 
-opt_lamb_beta_1 = 0.9
-opt_lamb_beta_2 = 0.999
-
 eps: float = 1e-08
 
 seed = 23333
-
-skip_evaluator = False
-debug_precision = False
-debug_cpu = False
