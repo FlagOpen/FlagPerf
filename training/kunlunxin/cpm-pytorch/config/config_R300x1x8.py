09078efd14cecfb28d9a3516efa504de13812a2d
Feilei Du <dufeilei@foxmail.com>
2023-05-18 06:11:09 +0000

    support cpm xpu

--- training/kunlunxin/cpm-pytorch/config/config_R300x1x8.py
+++ training/kunlunxin/cpm-pytorch/config/config_R300x1x8.py
@@ -2,11 +2,6 @@ from config_common import *
 
 dist_backend = "xccl"
 
-use_env = True
-target_embedding_average = 0.92
-
-gradient_accumulation_steps = 1
-
 train_batch_size = 32
 eval_batch_size = train_batch_size
 max_steps = 4000
@@ -14,10 +9,5 @@ max_samples_termination = 4391260
 
 warmup = 0.2
 learning_rate = 0.0005
-
-beta_1: float = 0.9
-beta_2: float = 0.99
-
-eps: float = 1e-08
 
 seed = 23333
