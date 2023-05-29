80f74d2527b30f1ef536659d0192566f4f2abd82
shh2000 <13820618441@163.com>
2023-05-29 13:25:42 +0800

    rm foo code

--- training/benchmarks/bigtransfer/pytorch/train/training_state.py
+++ training/benchmarks/bigtransfer/pytorch/train/training_state.py
@@ -6,27 +6,17 @@ import torch
 @dataclass
 class TrainingState:
     """TrainingState dataclass"""
-    _trainer = None
-    _status = 'aborted'  # later set to 'success' if termination criteria met
-
     global_steps = 0
 
     loss: float = 0.0
     eval_mAP: float = 0.0
 
     epoch: int = 1
-    num_trained_samples = 0
     end_training: bool = False
     converged: bool = False
 
     init_time = 0
     raw_train_time = 0
-
-    def status(self):
-        """get status"""
-        if self.converged:
-            self._status = "success"
-        return self._status
 
     def converged_success(self):
         """converged success"""
