0a783044253ecad83cc846ccc38c06bb5d926906
UP <wwlwenli@163.com>
2023-05-31 10:48:58 +0800

    optimize according to review comments

--- training/nvidia/wav2vec2-pytorch/config/config_A100x1x8.py
+++ training/nvidia/wav2vec2-pytorch/config/config_A100x1x8.py
@@ -1,21 +1,11 @@
-# perf
-ddp_type: str = "native"
-target_acc = 0.605
-do_train = True
-device: str = None
-n_device: int = 1
-log_frequency = log_freq = 1
-local_rank: int = -1
-seed: int = 1
+vendor = "nvidia"
 
-#important set
-resume = False  #[bool], default False, if True, read last_checkpoint from ckpt saved path
-ckpt = None  #[str,path], default None, if True, given ckpt path
-no_save = True  #[bool], default True, if True , do not save ckpt
-save_frequency = 1
+dist_backend: str = "nccl"
 
-# IO
-output_dir = "results/pretrain_base"
-data_dir = None
-train_subset = "train-full-960"
-valid_subset = "dev-other"
+# Batching
+num_concat_batches = 4  #Keep NUM_NODES x $NUM_GPUS x $NUM_CONCAT_BATCHES x $UPDATE_FREQ = 64
+update_freq = 2  # This config is for 1 NODE, 8 GPUS, so NUM_CONCAT_BATCHES=4, UPDATE_FREQ=2
+max_sample_size = 250000
+min_sample_size = 32000
+max_tokens = 1400000
+eval_steps = 100
