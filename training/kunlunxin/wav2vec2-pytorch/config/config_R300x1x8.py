d4648b8b98b20216ccd20af161e2f3d3fc3b8b7e
Feilei Du <dufeilei@foxmail.com>
2023-06-07 02:58:47 +0000

    add xpu case wav2vec2

--- training/kunlunxin/wav2vec2-pytorch/config/config_R300x1x8.py
+++ training/kunlunxin/wav2vec2-pytorch/config/config_R300x1x8.py
@@ -3,8 +3,8 @@ vendor = "kunlunxin"
 dist_backend: str = "xccl"
 
 # Batching
-num_concat_batches = 1  #Keep NUM_NODES x $NUM_GPUS x $NUM_CONCAT_BATCHES x $UPDATE_FREQ = 64
-update_freq = 1  # This config is for 1 NODE, 8 GPUS, so NUM_CONCAT_BATCHES=4, UPDATE_FREQ=2
+num_concat_batches = 4  #Keep NUM_NODES x $NUM_GPUS x $NUM_CONCAT_BATCHES x $UPDATE_FREQ = 64
+update_freq = 2  # This config is for 1 NODE, 8 GPUS, so NUM_CONCAT_BATCHES=4, UPDATE_FREQ=2
 max_sample_size = 250000
 min_sample_size = 32000
 max_tokens = 1400000
