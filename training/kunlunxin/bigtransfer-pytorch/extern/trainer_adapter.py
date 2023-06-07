8f237c2d347cfdf78bf6d7235ce849f2bf26259c
WZD09 <2101210655@stu.pku.edu.cn>
2023-06-07 15:31:56 +0800

    fix

--- training/kunlunxin/bigtransfer-pytorch/extern/trainer_adapter.py
+++ training/kunlunxin/bigtransfer-pytorch/extern/trainer_adapter.py
@@ -1,9 +1,6 @@
 from torch import nn
 import torch.distributed as dist
 
-def model_to_fp16(model: nn.Module):
-    return model
-
 def model_to_ddp(model: nn.Module) -> nn.Module:
     if dist.is_available() and dist.is_initialized():
         from torch.nn.parallel import DistributedDataParallel as DDP
