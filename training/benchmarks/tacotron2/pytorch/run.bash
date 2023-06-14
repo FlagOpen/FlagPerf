3ab8e4fead6bfd89c10d7e934ae592367d00c34e
Eddiewdy <1206240111@qq.com>
2023-06-14 03:32:51 +0000

    add extern

--- training/benchmarks/tacotron2/pytorch/run.bash
+++
@@ -1,8 +0,0 @@
-export XPU_VISIBLE_DEVICES=2 
-python run_pretraining.py \
-    --vendor kunlunxin \
-    --extern_config_dir=/fp/toremove/FlagPerf/training/kunlunxin/tacotron2-pytorch/config \
-    --extern_config_file=config_common.py \
-    --enable_extern_config \
-    --extern_module_dir=/fp/toremove/FlagPerf/training/kunlunxin/tacotron2-pytorch/extern \
-    
