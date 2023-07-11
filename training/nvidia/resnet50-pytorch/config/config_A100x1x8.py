6783ff3617761f1a25aeacc604ca75124bf2438d
SHIHONGHAO <13820618441@163.com>
2023-07-11 09:53:40 +0800

    refactor resnet50 (#127)
    * refactor resnet50
    
    * upd res
    
    * Update README.md

--- training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
+++ training/nvidia/resnet50-pytorch/config/config_A100x1x8.py
@@ -1,9 +1,4 @@
-from config_common import *
-
-seed = 23333
-
-learning_rate = 0.1
+lr = 0.1
 train_batch_size = 32
-eval_batch_size = train_batch_size
-# total samples of 100 epoch for iterating ILSVRC2012 dataset 
-max_samples_termination = 133116700
+eval_batch_size = 32
+
