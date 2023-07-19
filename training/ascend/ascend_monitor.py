bc833cfd85b48869f1549424275b1a120e44f3ff
fanxiaotong <fanxiaotong32@163.com>
2023-07-19 14:22:59 +0800

    fix bug

--- training/ascend/ascend_monitor.py
+++ training/ascend/ascend_monitor.py
@@ -75,7 +75,7 @@ class Daemon:
 
         def npu_mon(file):
             TIMESTAMP = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-            cmd = "npu-smi  info -m | egrep -v 'ID' | awk '{print $1" "$2" "$3}'| xargs"
+            cmd = "npu-smi  info"
             process = subprocess.Popen(cmd,
                                        shell=True,
                                        stdout=subprocess.PIPE,
--- training/benchmarks/resnet50/mindspore/code/eval.py
+++ training/benchmarks/resnet50/mindspore/code/eval.py
@@ -88,9 +88,11 @@ def eval_net():
     elif not os.path.isdir(ACC_DIR):
         print("Warning: The environment variable 'RESULT_PATH' is not a valid directory. ")
     else:
+        if res['top_1_accuracy'] < config.target_acc1:
+            CONVERGED=False
         ACC_LOG = os.path.join(ACC_DIR, "info.log")
         with open(ACC_LOG, 'a') as f:
-            f.write("{}".format(res['top_1_accuracy']))
+            f.write("final_acc1:{}".format(res['top_1_accuracy']))
 
 if __name__ == '__main__':
     eval_net()
