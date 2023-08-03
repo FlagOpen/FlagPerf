1b365f2a2db5e7939a1a3b16aecb899f509c80ba
shh2000 <13820618441@163.com>
2023-08-03 17:37:44 +0800

    fix

--- inference/benchmarks/bertLarge/pytorch/dataloader.py
+++ inference/benchmarks/bertLarge/pytorch/dataloader.py
@@ -6,18 +6,16 @@ import random
 
 class BertInferDataset(Dataset):
 
-    def __init__(self, input_ids, label_ids, random_dupo, seq_length):
+    def __init__(self, input_ids, label_ids, seq_length):
         self.input_ids = input_ids
         self.label_ids = label_ids
-        self.random_dupo = random_dupo
         self.seq_length = seq_length
 
     def __len__(self):
-        return len(self.input_ids) // self.seq_length * self.random_dupo
+        return len(self.input_ids) // self.seq_length
 
     def __getitem__(self, idx):
-        idx_global = idx // self.random_dupo
-        start_idx = idx_global * self.seq_length
+        start_idx = idx * self.seq_length
         chunk_input = self.input_ids[start_idx:start_idx + self.seq_length]
         chunk_label = self.label_ids[start_idx:start_idx + self.seq_length]
 
@@ -49,8 +47,7 @@ def build_dataset(config):
     input_ids = tokenizer.convert_tokens_to_ids(masked_tokens)
     input_ids = [tokenizer.cls_token_id] + input_ids + [tokenizer.sep_token_id]
 
-    dataset = BertInferDataset(input_ids, label_ids, config.random_dupo,
-                               config.seq_length)
+    dataset = BertInferDataset(input_ids, label_ids, config.seq_length)
 
     return dataset
 
