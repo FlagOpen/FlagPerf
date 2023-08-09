62134bb0adbb7b4d18e5cfafc5b228bae191a12f
clveryang <50865584+clveryang@users.noreply.github.com>
2023-08-09 12:22:08 +0800

    Iluvatar VisionTransformer repo (#188)
    * Iluvatar Bigtransfer case
    
    * iluvatar transformer

---
+++ training/iluvatar/vit-pytorch/config/config_common.py
@@ -0,0 +1,18 @@
+vendor = "iluvatar"
+dist_backend = "nccl"
+
+epochs = 300
+opt = "adamw"
+lr = 0.003
+weight_decay = 0.3
+lr_scheduler = "cosineannealinglr"
+lr_warmup_method = "linear" 
+lr_warmup_epochs = 30
+lr_warmup_decay = 0.033 
+amp = True 
+label_smoothing = 0.11
+mixup_alpha = 0.2
+auto_augment = "ra"
+clip_grad_norm = 1
+ra_sampler = True
+cutmix_alpha = 1.0
