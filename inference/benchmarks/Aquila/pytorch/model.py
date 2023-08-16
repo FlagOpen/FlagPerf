023040d9b349ea61c4b683d5927264c4e30156ee
UP <wwlwenli@163.com>
2023-08-16 14:20:04 +0800

    support aquila7b

---
+++ inference/benchmarks/Aquila/pytorch/model.py
@@ -0,0 +1,33 @@
+import os
+
+import torch
+from loguru import logger
+
+from flagai.auto_model.auto_loader import AutoLoader
+
+
+def create_model(config):
+    logger.info("build model...")
+    if os.path.exists(config.model_save_path):
+        model_dict = torch.load(config.model_save_path)
+        model = model_dict["model"]
+        tokenizer = model_dict["tokenizer"]
+    else:
+        loader = AutoLoader(
+        "lm",
+        model_dir=config.state_dict,
+        model_name=config.model_name,
+        use_cache=True,
+        fp16=True)
+        model = loader.get_model()
+        tokenizer = loader.get_tokenizer()
+        torch.save({"model":model, "tokenizer":tokenizer}, config.model_save_path)
+
+    model.cuda()
+    model.eval()
+    if config.fp16:
+        model.half()
+
+    return (model, tokenizer)
+
+
