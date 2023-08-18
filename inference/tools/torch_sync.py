58bed1ad50e529eed45e7a10c38be1ea68e92538
stezpy <stezpy@gmail.com>
2023-08-18 10:57:22 +0800

    Iluvatar inference Resnet50 (#195)
    * add ixrt
    
    * add torch sync
    
    * customized input & output
    
    * merge latest
    
    * update
    
    * update readme
    
    * update readme
    
    * update
    
    ---------
    
    Co-authored-by: stezpy <peiyuan.zhang@iluvatar.com>

--- inference/tools/torch_sync.py
+++ inference/tools/torch_sync.py
@@ -4,6 +4,8 @@ import torch
 def torch_sync(config):
     if config.vendor == "nvidia":
         torch.cuda.synchronize()
+    if config.vendor == "iluvatar":
+        torch.cuda.synchronize()
     if config.vendor == "kunlunxin":
         # kunlunxin case
         # xpu sync already finsh after InferModel.__call__
