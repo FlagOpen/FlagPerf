01b43aae41a9d8c3c3e11f30c99734008fbc691f
shh2000 <13820618441@163.com>
2023-08-21 14:23:14 +0800

    rm

--- inference/benchmarks/swinTransformer_tiny/pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dataloader import build_dataloader
-from .model import create_model
-from .export import export_model
-from .evaluator import evaluator
-from .forward import model_forward, engine_forward
