ce486628f479a226617ba2397658343f4da1cca6
zhangytong04 <zhangytong04@baidu.com>
2023-08-30 15:09:13 +0800

    一键run.py更新

--- training/kunlunxin/cpm-pytorch/config/environment_variables.sh
+++ training/kunlunxin/cpm-pytorch/config/environment_variables.sh
@@ -20,9 +20,17 @@ export BKCL_CCIX_RING="1"
 export ALLREDUCE_ASYNC="0"
 export ALLREDUCE_FUSION="0"
 
-pip install sentencepiece jieba numpy
+KLX_WEB_SERVER_URL=https://bd.bcebos.com/klx-pytorch-ipipe-bd
 
-bos_http=https://bd.bcebos.com/klx-pytorch-ipipe-bd/
+wget --tries=1 --read-timeout=1 10.1.2.158:8111/hello.txt || true
+if [ -e hello.txt ]; then
+    if [ "$(cat hello.txt)" = "hello from 158!" ]; then
+        KLX_WEB_SERVER_URL=http://10.1.2.158:8111
+    fi
+fi
 
-pip install ${KLX_WEB_SERVER_URL:-${bos_http}}flagperf/202307/cpm/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
-pip install ${KLX_WEB_SERVER_URL:-${bos_http}}flagperf/202307/cpm/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
+echo "KLX_WEB_SERVER_URL is set to $KLX_WEB_SERVER_URL"
+rm -f hello.txt
+
+pip install ${KLX_WEB_SERVER_URL}/flagperf/202307/cpm/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
+pip install ${KLX_WEB_SERVER_URL}/flagperf/202307/cpm/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
