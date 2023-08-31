035d3781c4a853c0916e778fbce64f766bc1f1ed
zhangytong04 <zhangytong04@baidu.com>
2023-08-31 14:34:46 +0800

    Launch 'run.py' with a single command.

--- training/kunlunxin/docker_image/pytorch/pytorch_install.sh
+++ training/kunlunxin/docker_image/pytorch/pytorch_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# set -xe
+set -xe
 
-# wget https://klx-public.bj.bcebos.com/xmlir/flagopen/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl -O ~/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
-# pip install ~/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
-# rm ~/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
+pip install https://bd.bcebos.com/klx-pytorch-ipipe-bd/flagperf/111e7d45/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
+pip install https://bd.bcebos.com/klx-pytorch-ipipe-bd/flagperf/111e7d45/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
