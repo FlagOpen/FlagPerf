44d6de9549ce1ea7c05f57102ff1849bcfbc9f02
KungYork <wangyakaizy@163.com>
2023-08-31 08:44:09 +0000

    Add cpm 1x1 2x8 configs

---
+++ training/kunlunxin/cpm-pytorch/config/config_R300x2x8.py
@@ -0,0 +1,13 @@
+from config_common import *
+
+dist_backend = "xccl"
+
+train_batch_size = 64
+eval_batch_size = train_batch_size
+max_steps = 700
+max_samples_termination = 4391260
+
+warmup = 0.2
+learning_rate = 0.0005
+
+seed = 23333
