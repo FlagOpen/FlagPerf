8fe18ac2a88cd76db842156015c3551fc048db5d
KungYork <30741085+KungYork@users.noreply.github.com>
2023-09-04 10:24:07 +0800

    Update environment_variables.sh for kunlunxin-cpm

--- training/kunlunxin/cpm-pytorch/config/environment_variables.sh
+++ training/kunlunxin/cpm-pytorch/config/environment_variables.sh
@@ -16,6 +16,7 @@ touch topo_file
 export XPUSIM_TOPOLOGY_FILE=$(readlink -f $topo_file)
 
 ## workaround due to ccix bug
+export BKCL_PCIE_RING=1
 export ALLREDUCE_ASYNC="0"
 export ALLREDUCE_FUSION="0"
 export BKCL_FORCE_SYNC=1
