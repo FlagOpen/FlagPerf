269effe4f3ffec09d6144ea12074c0120ec6b28f
GGuanl <143151018+GGuanl@users.noreply.github.com>
2023-09-07 15:07:22 +0800

    kunlunxin update glm config (#236)
    * glm_config
    
    * fix_#1
    
    * glm-config_updated
    
    * glm-config-updated#2
    
    * glm_config-updated#2
    
    * glm_config-#2
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update pytorch_install.sh
    
    * Create config_common
    
    * Update README.md
    
    * Rename config_common to config_common.py
    
    * Update config_R300x2x8.py
    
    * Update config_R300x1x1.py
    
    * Update config_R300x1x8.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update requirements.txt
    
    * Update README.md
    
    * Update config_R300x1x1.py
    
    * Update config_R300x1x8.py
    
    * Update config_R300x2x8.py
    
    * Update config_R300x1x1.py
    
    * Update config_R300x1x8.py
    
    * Update config_R300x2x8.py
    
    * Update config_common.py
    
    * Update config_R300x1x1.py
    
    * Update config_R300x2x8.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update config_R300x1x1.py
    
    * Update config_R300x1x8.py
    
    * Update config_R300x2x8.py
    
    ---------
    
    Co-authored-by: guanlongjie <guanlongjie@MacBook-Pro.local>

--- training/kunlunxin/glm-pytorch/config/environment_variables.sh
+++ training/kunlunxin/glm-pytorch/config/environment_variables.sh
@@ -7,6 +7,11 @@ export BKCL_TIMEOUT=1800
 # when using tree allreduce, the number of nodes must be a multiple of 2
 export BKCL_SOCKET_FORCE_TREE=1
 
+export XMLIR_D_XPU_L3_SIZE=32505856
+
+export BKCL_CCIX_RING=1
+export BKCL_FORCE_SYNC=1
+
 export ALLREDUCE_ASYNC=false
 export ALLREDUCE_FUSION=0
 
