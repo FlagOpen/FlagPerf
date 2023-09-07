186ae5de861d4d146a4b144c2aaf6c0c87389c62
GGuanl <143151018+GGuanl@users.noreply.github.com>
2023-09-07 13:39:56 +0800

    Update config_R300x2x8.py

--- training/kunlunxin/glm-pytorch/config/config_R300x2x8.py
+++ training/kunlunxin/glm-pytorch/config/config_R300x2x8.py
@@ -2,3 +2,16 @@ from config_common import*
 
 train_batch_size = 4
 eval_batch_size = 4
+
+lr = 1e-5
+weight_decay = 0.1
+adam_beta1 = 0.9
+adam_beta2 = 0.999
+adam_eps = 1e-08
+gradient_accumulation_steps = 1
+warmup = 0.1
+lr_decay_ratio = 0.1
+lr_decay_iters = 4338
+log_freq = 1
+seed = 4096
+max_samples_termination = 5553080
