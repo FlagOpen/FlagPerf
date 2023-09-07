5bbc318dbb9de94bc923fbc14324a24ecda0111c
zhouyu <zhouyu@baai.ac.cn>
2023-09-07 15:26:18 +0800

    update cpm 1x1 running stats

--- training/nvidia/cpm-pytorch/config/config_A100x1x1.py
+++ training/nvidia/cpm-pytorch/config/config_A100x1x1.py
@@ -3,13 +3,13 @@ from config_common import *
 
 fp16 = True
 dist_backend = "nccl"
-target_embedding_average = 0.8
+target_embedding_average = 0.92
 
 gradient_accumulation_steps = 1
 
 train_batch_size = 32
 eval_batch_size = train_batch_size
-max_steps = 60000
+max_steps = 3000
 max_samples_termination = 439126000
 
 warmup = 0.2
