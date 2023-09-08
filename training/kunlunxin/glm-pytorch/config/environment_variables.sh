f603d5ffe510ecb70b264ebeb2c3865e94917344
GGuanl <143151018+GGuanl@users.noreply.github.com>
2023-09-08 11:05:42 +0800

    Update environment_variables.sh

--- training/kunlunxin/glm-pytorch/config/environment_variables.sh
+++ training/kunlunxin/glm-pytorch/config/environment_variables.sh
@@ -17,3 +17,27 @@ export ALLREDUCE_FUSION=0
 
 export XMLIR_F_XPU_FC_GEMM_MODE=float
 export XMLIR_F_FAST_INDEX_PUT=true
+
+
+#export https_proxy=http://10.1.0.34:7890
+#export http_proxy=http://10.1.0.34:7890
+#export all_proxy=socks5h://10.1.0.34:7891
+
+KLX_WEB_SERVER_URL=https://bd.bcebos.com/klx-pytorch-ipipe-bd
+
+wget --tries=1 --read-timeout=1 10.1.2.158:8111/hello.txt || true
+if [ -e hello.txt ]; then
+    if [ "$(cat hello.txt)" = "hello from 158!" ]; then
+        KLX_WEB_SERVER_URL=http://10.1.2.158:8111
+    fi
+fi
+
+echo "KLX_WEB_SERVER_URL is set to $KLX_WEB_SERVER_URL"
+rm -fv hello.txt
+#https://bd.bcebos.com/klx-pytorch-ipipe-bd/flagperf/archives/111e7d45/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
+#https://bd.bcebos.com/klx-pytorch-ipipe-bd/flagperf/archives/111e7d45/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
+pip install ${KLX_WEB_SERVER_URL}/flagperf/archives/111e7d45/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl
+pip install ${KLX_WEB_SERVER_URL}/flagperf/archives/111e7d45/xacc-0.1.0-cp38-cp38-linux_x86_64.whl
+
+export XACC_ENABLE=1
+python -m xacc.install
