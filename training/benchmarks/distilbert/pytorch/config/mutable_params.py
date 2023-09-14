2d6b13845501121e774e06a05a71fbc549b49bd8
KungYork <wangyakaizy@163.com>
2023-09-14 16:35:24 +0800

    Add 1x1 2x8 cases

--- training/benchmarks/distilbert/pytorch/config/mutable_params.py
+++ training/benchmarks/distilbert/pytorch/config/mutable_params.py
@@ -1,5 +1,6 @@
 mutable_params = [
-    'vendor', 'data_dir', 'lr', 'weight_decay', 'train_batch_size',
-    'eval_batch_size', 'do_train', 'distributed', 'dist_backend', 'device',
-    'cudnn_benchmark', 'cudnn_deterministic'
+    'vendor', 'data_dir', 'lr', 'weight_decay', 'train_batch_size', 
+    'gradient_accumulation_steps', 'eval_batch_size', 'do_train', 
+    'distributed', 'dist_backend', 'device', 'cudnn_benchmark', 
+    'cudnn_deterministic'
 ]
