30c9665f4177cd4b018dc7c6626b116da3ac5c52
DrownFish19 <DrownFish19@gmail.com>
2023-09-18 15:48:13 +0800

    update base config

--- training/benchmarks/gpt3-6.7b/paddle/config/_base.py
+++ training/benchmarks/gpt3-6.7b/paddle/config/_base.py
@@ -65,32 +65,19 @@ pad_token_id = 0
 
 use_cache = False
 
-recompute = False
-
 tensor_parallel_output = True
 
 tie_word_embeddings = False
 
-use_flash_attention: bool = False
-
 # Pretrained tokenizer name or path if not the same as model_name
 tokenizer_name_or_path = None
-
-# llama, use_fused_rms_norm
-use_fused_rms_norm: bool = False
-
-# gpt, fuse_attention_qkv
-fuse_attention_qkv: bool = False
-
-fuse_attention_ffn: bool = False
-
-# full core_attn
-recompute_granularity: str = "full"
 
 # virtual_pp_degree
 virtual_pp_degree: int = 1
 
-# Pre-training from existing paddlenlp model weights. Default Fasle and model will train from scratch. If set True, the model_name_or_path argument must exist in the paddlenlp models.
+# Pre-training from existing paddlenlp model weights. 
+# Default Fasle and model will train from scratch. 
+# If set True, the model_name_or_path argument must exist in the paddlenlp models.
 continue_training: bool = False
 
 num_workers: int = 1
@@ -156,7 +143,28 @@ seed: int = 42
 
 max_grad_norm: float = 1.0
 
+# =========================================================
+# parallel args
+# =========================================================
+
+recompute = False
+
+use_flash_attention: bool = False
+
+# 需要安装[此目录](https://github.com/PaddlePaddle/PaddleNLP/tree/develop/model_zoo/gpt-3/external_ops)下的自定义OP
+use_fused_rms_norm: bool = False
+
+# fuse_attention_qkv
+fuse_attention_qkv: bool = False
+
+fuse_attention_ffn: bool = False
+
+# full core_attn
+recompute_granularity: str = "full"
+
 sharding: str = "stage2"
+
+sharding_degree: int = -1
 
 tensor_parallel_degree = 1
 
