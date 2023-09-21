4916461f1c93a520adaead5d2dabc8d90d5877e9
feldmanshan <feldmanshan@tencent.com>
2023-09-21 14:40:07 +0800

    zixiao:add resnet50 inference configs && results

---
+++ inference/docker_images/zixiao/zixiao_analysis.py
@@ -0,0 +1,19 @@
+def analysis_log(logpath):
+    logfile = open(logpath)
+
+    max_usage = 0.0 ## usage_mem
+    max_mem = 16.0 
+    for line in logfile.readlines():
+        '''
+        zx-smi pwr DTemp MUsed Mem
+        '''
+        if "zx-smi" in line:
+            line = line[:-1]
+            usage = line.split(" ")[3]
+            usage = float(usage)*16/100
+            max_usage = max(max_usage, usage)
+    return round(max_usage, 2), max_mem, eval("30e12"), eval("120e12")
+
+
+if __name__ == "__main__":
+    max1, max2, max2,max4 = analysis_log("zixiao.log")
