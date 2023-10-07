f09f1a5265ba23dc1e6f1a3d82c9f1677083cf68
GGuan123 <guanlongjie@baidu.com>
2023-10-07 11:14:32 +0800

    fix_#5

--- training/kunlunxin/transformer_xl-pytorch/config/environment_variables.sh
+++ training/kunlunxin/transformer_xl-pytorch/config/environment_variables.sh
@@ -1,6 +1,3 @@
 export XACC_ENABLE=1
 export BKCL_PCIE_RING=1
 
-pip install /data/GGuan/FlagPerf/training/dist/xacc-0.1.0-cp38-cp38-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install /data/GGuan/FlagPerf/training/dist/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
-python3 -m xacc.install
