361f13ba0d1698bf11eecfa3b2d846a89feb02d7
zhouyu <zhouyu@baai.ac.cn>
2023-10-07 17:04:51 +0800

    remove proxy info from env.sh

--- training/nvidia/dlrm-pytorch/config/environment_variables.sh
+++ training/nvidia/dlrm-pytorch/config/environment_variables.sh
@@ -2,7 +2,3 @@
 # Export variables
 # =================================================
 export OMP_NUM_THREADS=8
-
-export https_proxy=http://10.1.0.34:7890 
-export http_proxy=http://10.1.0.34:7890 
-export all_proxy=socks5h://10.1.0.34:7891
