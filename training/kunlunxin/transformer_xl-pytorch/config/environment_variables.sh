0ced700a2e00b4389aacf9cecf52f82848ac934a
GGuan123 <guanlongjie@baidu.com>
2023-10-07 10:58:52 +0800

    fix_#2

--- training/kunlunxin/transformer_xl-pytorch/config/environment_variables.sh
+++ training/kunlunxin/transformer_xl-pytorch/config/environment_variables.sh
@@ -1,10 +1,6 @@
 export XACC_ENABLE=1
+export BKCL_PCIE_RING=1
 
-pip install datasets -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install transformers -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install sacremoses -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install psutil -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install accelerate -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install /data/GGuan/FlagPerf/training/dist/xacc-0.1.0-cp38-cp38-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install /data/GGuan/FlagPerf/training/dist/xmlir-0.0.1-cp38-cp38-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
 python3 -m xacc.install
