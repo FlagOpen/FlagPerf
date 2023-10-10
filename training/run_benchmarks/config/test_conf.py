410b73b97db2c7be0ebfa3a429ee8a8374810ba1
Reiase <reiase@gmail.com>
2023-10-10 16:19:44 +0800

    add longformer training stdcase (#282)
    * add longformer
    
    * fix typos in README.md

--- training/run_benchmarks/config/test_conf.py
+++ training/run_benchmarks/config/test_conf.py
@@ -78,6 +78,7 @@ CASES = {
     # "transformer_xl:pytorch_1.8:A100:1:8:1": "/raid/dataset/transformer_xl/",
     # "t5_small:pytorch_1.8:A100:1:8:1": "/home/datasets_ckpt/t5_small_train",
     # "gpt2:pytorch_1.12:A100:1:8:1": "/raid/dataset/gpt2",
+    # "longformer:pytorch_1.12:A100:1:8:1": "/raid/dataset/longformer_train/",
     
     # "llama1_7B:paddle_2.5.1:TP1PP1SH2SP8A10040G:1:8:1":"/raid/dataset/llama/"
     # "llama1_7B:paddle_2.5.1:TP2PP1SH1SP4A10040G:1:8:1":"/raid/dataset/llama/"
