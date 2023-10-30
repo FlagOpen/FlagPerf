50e4871cd17aea2a877df4b674f5858ff7044567
GGuan123 <guanlongjie@baidu.com>
2023-10-30 16:03:41 +0800

    longformer_fix#8

--- training/kunlunxin/longformer-pytorch/config/environment_variables.sh
+++ training/kunlunxin/longformer-pytorch/config/environment_variables.sh
@@ -1,7 +1,6 @@
 export XACC_ENABLE=1
 export BKCL_PCIE_RING=1
 export XMLIR_D_FORCE_FALLBACK_STR="aten::index_add_"
-export XMLIR_FALLBACK_OP_LIST_FILE_PATH="log"
 export XMLIR_XPU_EAGER_LAUNCH_SYNC_MODE=true
 
 python3 -m xacc.install
