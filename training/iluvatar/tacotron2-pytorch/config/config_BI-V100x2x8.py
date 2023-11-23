0da67343095fa96ef2a334e39249672ff7c47cd1
魏杰 <jie.wei@iluvatar.com>
2023-11-23 17:03:43 +0800

    update iluvatar tacotron2 config

--- training/iluvatar/tacotron2-pytorch/config/config_BI-V100x2x8.py
+++ training/iluvatar/tacotron2-pytorch/config/config_BI-V100x2x8.py
@@ -1,9 +1,11 @@
 from config_common import *
 
-train_batch_size = 64
+train_batch_size = 96
 eval_batch_size = train_batch_size
 
 warmup = 0.2
 learning_rate = 1e-3
 
 seed = 23333
+
+max_epochs = 5
