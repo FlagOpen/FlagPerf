a751f07824cbc581c0435da2c3962d65d75b8581
shh2000 <13820618441@163.com>
2023-12-04 15:35:21 +0800

    rm privacy

--- training/run_benchmarks/in_container/config.py
+++ training/run_benchmarks/in_container/config.py
@@ -1,9 +1,8 @@
-flagperf_home = "/home/shihonghao/multinode/FlagPerf/"
+flagperf_home = "/home/FlagPerf/"
 casename = "aquila2_7B_container"
 vendor = "nvidia"
 logdir = "result"
 
 # the first ip in "hosts" is master, others is workers
-#hosts = ["10.1.2.155"]
-hosts = ["10.1.2.155", "10.1.2.158"]
+hosts = ["192.168.1.2", "192.168.1.3"]
 master_port = 29501
