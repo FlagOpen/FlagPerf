8e748d2587b69ae9b7a5337a219353a23ab9f149
shh2000 <13820618441@163.com>
2023-12-06 17:02:58 +0800

    123123

--- training/run_benchmarks/in_container/run.py
+++ training/run_benchmarks/in_container/run.py
@@ -35,9 +35,9 @@ def main():
 
     entrance_path = os.path.dirname(entrance_file)
 
-    log_dir = os.path.join(flagperf_home, "training", log_dir)
+    logdir = os.path.join(flagperf_home, "training", log_dir)
     timestamp_str = (datetime.now()).strftime("%Y%m%d%H%M%S")
-    log_dir_stamp = os.path.join(log_dir, "run" + timestamp_str)
+    log_dir_stamp = os.path.join(logdir, "run" + timestamp_str)
     os.makedirs(log_dir_stamp, exist_ok=True)
 
     exec_cmd = "cd " + entrance_path + ";"
