8d7314594e3c4421893f630565564e4741fdc942
linlang1837 <45357704+linlang1837@users.noreply.github.com>
2023-12-15 14:01:32 +0800

    [kunlunxin] fix bigtransfer and add configs (#363)
    * add 1x8 and 1x1 config, add requirements.txt and environment_variables.sh, add test case, fix README
    
    * fix val_loss in README
    
    * fix the table in readme
    
    * fix the number "final_acc1" in readme
    
    ---------
    
    Co-authored-by: jianglinlang <jianglinlang@baidu.com>

--- training/run_benchmarks/config/test_conf.py
+++ training/run_benchmarks/config/test_conf.py
@@ -124,6 +124,7 @@ CASES = {
     # "distilbert:pytorch:R300:1:8:1": "/raid/dataset/distilbert/",
     # "swin_transformer:pytorch:R300:1:8:1": "/raid/dataset/ImageNet_1k_2012/",
     # "tacotron2:pytorch:R300:1:8:1": "/raid/dataset/tacotron2/LJSpeech/",
+    # "bigtransfer:pytorch:R300:1:8:1": "/raid/dataset/ImageNet_1k_2012/",
 
     # mthreads cases
     # "resnet50:pytorch_2.0:S4000:1:8:1": "/data/flagperf/ImageNet",
