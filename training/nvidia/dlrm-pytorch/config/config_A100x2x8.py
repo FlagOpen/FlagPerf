4f7f43128c695481b8bfa4165c307cfb5847ce0e
zhouyu <zhouyu@baai.ac.cn>
2023-12-18 18:07:17 +0800

    add 1x1,2x8 official bs.

--- training/nvidia/dlrm-pytorch/config/config_A100x2x8.py
+++ training/nvidia/dlrm-pytorch/config/config_A100x2x8.py
@@ -1,6 +1,6 @@
 from config_common import *
 
 # Batch size used for training
-train_batch_size: int = 2752512
+train_batch_size: int = 65536
 # Batch size used for testing/validation
-eval_batch_size: int = 2752512
+eval_batch_size: int = 65536
