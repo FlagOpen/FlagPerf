6c0cbd8dacaa8fa2eab195c1adb61fffd5ab0c64
shh2000 <13820618441@163.com>
2023-12-18 10:40:46 +0800

    Aquila 34/70B (#364)
    * init
    
    * 123123
    
    * rm privacy
    
    * fixnet
    
    * monitor
    
    * 123123
    
    * fix
    
    * add req
    
    * 123123
    
    * 123132
    
    * 23123
    
    * 123123
    
    * 123123
    
    * 123123
    
    * 123123
    
    * 23123
    
    * try-3470

---
+++ training/benchmarks/aquila2_70B_container/in_container/singlenode_run.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+ifconfig
+
+git log | head -n 5
+
+SCALEHOME=$1
+
+DATADIR=$2
+DATASET=$3
+TRAININGSAMPLES=$4
+
+TP=$5
+PP=$6
+MBS=$7
+GBS=$8
+
+NODERANK=$9
+NNODES=${10}
+MASTERADDR=${11}
+MASTERPORT=${12}
+
+export PYTHONPATH=$PYTHONPATH:$SCALEHOME
+
+VOCAB_FILE=$SCALEHOME/examples/aquila/tokenizer/vocab.json
+MERGE_FILE=$SCALEHOME/examples/aquila/tokenizer/merges.txt
+SPECIAL_TOKENS_FILE=$SCALEHOME/examples/aquila/tokenizer/special_tokens.txt
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node 8 \
+    --nnodes $NNODES \
+    --node_rank $NODERANK \
+    --master_addr $MASTERADDR \
+    --master_port $MASTERPORT
+"
+
+TRAINING_ARGS="
+    --train-samples $TRAININGSAMPLES \
+    --eval-iters 0 \
+    --tensor-model-parallel-size $TP \
+    --pipeline-model-parallel-size $PP \
+    --micro-batch-size $MBS \
+    --global-batch-size $GBS \
+    --disable-bias-linear \
+    --use-distributed-optimizer \
+    --sequence-parallel \
+    --use-flash-attn
+"
+
+MIXED_PRECISION_ARGS="
+    --bf16 \
+    --embedding-weights-in-fp32 \
+    --rotary-position-embeddings-in-fp32 \
+    --attention-softmax-in-fp32 \
+    --accumulate-allreduce-grads-in-fp32
+"
+
+DATA_ARGS="
+    --data-path $DATADIR/$DATASET \
+    --tokenizer-type AquilaTokenizer \
+    --vocab-file $VOCAB_FILE \
+    --vocab-size 100008\
+    --merge-file $MERGE_FILE \
+    --special-tokens-file $SPECIAL_TOKENS_FILE \
+    --data-impl mmap \
+    --split 1
+"
+
+NETWORK_ARGS="
+    --num-layers 80 \
+    --hidden-size 8192 \
+    --num-attention-heads 64 \
+    --group-query-attention \
+    --num-query-groups 8 \
+    --hidden-dim-multiplier 1.3 \
+    --seq-length 4096 \
+    --max-position-embeddings 4096 \
+    --layernorm-epsilon 1e-5 \
+    --layernorm-init-weight 0.25 \
+    --use-rotary-position-embeddings \
+    --rotary-position-embeddings-in-fp32 \
+    --no-position-embedding \
+    --swiglu \
+    --multiple-of 4096 \
+    --apply-layernorm-rms \
+    --untie-embeddings-and-output-weights
+"
+
+INITIALIZATION_ARGS="
+    --init-method-std 0.0149 \
+    --seed 1234 
+"
+
+REGULARIZATION_ARGS="
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --weight-decay 0.1 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.95 \
+    --clip-grad 1.0
+"
+
+LEARNING_RATE_ARGS="
+    --lr 1.5e-4 \
+    --min-lr 1.5e-5 \
+    --lr-decay-style cosine \
+    --lr-warmup-samples 8
+"
+
+
+cmd="torchrun $DISTRIBUTED_ARGS $SCALEHOME/pretrain_gpt.py \
+              $TRAINING_ARGS \
+              $MIXED_PRECISION_ARGS \
+              $DATA_ARGS \
+              $NETWORK_ARGS \
+              $INITIALIZATION_ARGS \
+              $REGULARIZATION_ARGS \
+              $LEARNING_RATE_ARGS
+    "
+echo $cmd
+eval $cmd
