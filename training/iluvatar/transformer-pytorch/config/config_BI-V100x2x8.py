01c5c1de9d3841287ddf30bb2e6ae4eead0badf2
Zhou Yu <zycosmos@gmail.com>
2023-12-26 16:01:40 +0800

    Update config_BI-V100x2x8.py
    update comments. set max_epoch to 5

--- training/iluvatar/transformer-pytorch/config/config_BI-V100x2x8.py
+++ training/iluvatar/transformer-pytorch/config/config_BI-V100x2x8.py
@@ -3,5 +3,5 @@ from config_common import *
 max_tokens = 10240
 max_epoch = 30
 # The following configuration parameter is for performace test only
-# max_epoch = 1
+# max_epoch = 5
 lr = [0.000846]
