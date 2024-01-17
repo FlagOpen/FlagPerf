11e50f349c96ae84573d9f9534758335f368fae1
zhouyu <zhouyu@baai.ac.cn>
2024-01-17 17:32:23 +0800

    add case example for test_conf

--- training/nvidia/moflow-pytorch/config/environment_variables.sh
+++ training/nvidia/moflow-pytorch/config/environment_variables.sh
@@ -2,7 +2,7 @@
 # Export variables
 # =================================================
 # Add your own proxy here if you have problems connecting with github.com
-export http_proxy="10.1.0.34:7890"
-export https_proxy="10.1.0.34:7890"
+# export http_proxy="127.0.0.1:7890"
+# export https_proxy="127.0.0.1:7890"
 
 export OMP_NUM_THREADS=8
