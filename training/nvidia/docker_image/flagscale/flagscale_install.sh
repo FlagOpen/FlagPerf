d24a04ac93427d1422e93b704ec1d83a0139e306
zhouyu <zhouyu@baai.ac.cn>
2024-01-30 17:52:42 +0800

    update flagscale_instal.sh

--- training/nvidia/docker_image/flagscale/flagscale_install.sh
+++ training/nvidia/docker_image/flagscale/flagscale_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # using github mirrors to avoid github timeout issue
-commit_id="ed55532"
 git clone https://githubfast.com/FlagOpen/FlagScale
 cd FlagScale
-git reset --hard ${commit_id}
+git checkout -b FlagScale ed55532
 echo 'export PYTHONPATH=$PYTHONPATH:/workspace/FlagScale' >> /root/.bashrc
 source /root/.bashrc
