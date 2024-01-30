914b9db552773fa375de9801e40fcda2bfd25f3c
Shengchu Zhao <shengchu.zhao@metax-tech.com>
2024-01-30 12:39:51 +0800

    delete history

--- .history/training/benchmarks/faster_rcnn/pytorch/model/__init___20240121222426.py
+++
@@ -1,9 +0,0 @@
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-import torchvision
-
-
-def create_model():
-
-    torchvision.models.resnet.__dict__['model_urls'][
-        'resnet50'] = 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
-    return fasterrcnn_resnet50_fpn()
