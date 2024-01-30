b8704243b21cfad1d854d7cb20df5224b069cc72
zhouyu <zhouyu@baai.ac.cn>
2024-01-30 17:34:50 +0800

    set flagscale to fixed version.

--- training/nvidia/docker_image/flagscale/flagscale_install.sh
+++ training/nvidia/docker_image/flagscale/flagscale_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# using github mirrors to avoid github TTL
+# using github mirrors to avoid github timeout issue
+commit_id="ed55532"
 git clone https://githubfast.com/FlagOpen/FlagScale
+cd FlagScale
+git reset --hard ${commit_id}
 echo 'export PYTHONPATH=$PYTHONPATH:/workspace/FlagScale' >> /root/.bashrc
 source /root/.bashrc
