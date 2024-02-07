d1a078e0cf541ad691b2cbf7ba57f3576c4805a5
fred1912 <fred1912@users.noreply.github.com>
2024-02-07 16:01:45 +0800

    [metax]update main page (#448)
    * update readme
    
    * add company info
    
    * faster_rcnn update & first PR
    
    * fix readme
    
    * add config 1x8 bs=16
    
    * fix typo A100->C500
    
    * remove torchvision in requirements.txt
    
    * update readme
    
    * update fasterrcnn readme
    
    * update
    
    * add 2x8 info & add 带宽
    
    * fix typo
    
    * delete history
    
    * update info
    
    * update info
    
    * update table
    
    * delete history
    
    * add info in test-conf
    
    * fix typo
    
    * delete history
    
    * fix env bug & add mx tf32 env
    
    * update requirements
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * update file path
    
    * support torch2.0 and torchvision>0.12
    
    * delete history
    
    * add mext seed related
    
    * update main page
    
    * replate 5 files
    
    * ur
    
    * ur
    
    ---------
    
    Co-authored-by: Shengchu Zhao <shengchu.zhao@metax-tech.com>
    Co-authored-by: shh2000 <13820618441@163.com>

--- training/nvidia/docker_image/megatron/megatron_install.sh
+++ training/nvidia/docker_image/megatron/megatron_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # using github mirrors to avoid github TTL
 git clone https://githubfast.com/FlagOpen/FlagScale
+cd FlagScale
 git checkout 26cd6643c472f853e077779abaa51bb6a1c140bf
 echo 'export PYTHONPATH=$PYTHONPATH:/workspace/FlagScale' >> /root/.bashrc
 source /root/.bashrc
