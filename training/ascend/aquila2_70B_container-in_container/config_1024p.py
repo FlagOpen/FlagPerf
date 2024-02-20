8ad36623a826a9b31700a5dde36cd313d2c7f900
htwang <onehaitao@foxmail.com>
2024-02-20 18:58:50 +0800

    [Ascend] Update scripts for Aquila2

--- training/ascend/aquila2_70B_container-in_container/config_1024p.py
+++ training/ascend/aquila2_70B_container-in_container/config_1024p.py
@@ -12,8 +12,8 @@ DATA_DIR = "/data/wudao_pretrain"
 DATASET = "wudao_pretrain_text_document"
 FLAGSCALE_HOME = "/mnt/baai_test/wspace/FlagScale"
 # 1B tokens for nnodes=1, model=7B
-# 1000 steps * SEQLENGTH (4096) * GLOBAL_BATCHSIZE (1408) = 5767168000
-TRAINING_TOKENS = 5767168000
+# 1000 steps * SEQLENGTH (4096) * GLOBAL_BATCHSIZE (5632) = 23068672000
+TRAINING_TOKENS = 23068672000
 
 # =========================================================
 # parallel
@@ -27,7 +27,7 @@ PIPELINE_PARALLEL = 2
 MICRO_BATCHSIZE = 2
 # globalbs = microbs * gradient_accu_steps * (worldsize/tp/pp)
 # gradient_accu_steps is the same as flagscale aquila-7B(9)
-GLOBAL_BATCHSIZE = 1408
+GLOBAL_BATCHSIZE = 5632
 # 2k for aquila2-7B, 4k for aquila2-34B and 70B
 SEQLENGTH = 4096
 
@@ -39,4 +39,4 @@ flops_16bit = "313000000000000"
 # =========================================================
 # network
 # =========================================================
-net_cmd ="export HCCL_CONNECT_TIMEOUT=3600;export HCCL_EXEC_TIMEOUT=0;export GLOO_SOCKET_IFNAME=bond0;export HCCL_SOCKET_IFNAME=bond0;export CUDA_DEVICE_MAX_CONNECTIONS=1"
+net_cmd ="export HCCL_CONNECT_TIMEOUT=3600;export HCCL_EXEC_TIMEOUT=0;export GLOO_SOCKET_IFNAME=bond0;export HCCL_SOCKET_IFNAME=bond0;export CUDA_DEVICE_MAX_CONNECTIONS=1;ulimit -n 409600"
