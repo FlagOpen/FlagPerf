6777f05fa207c51366c2a1e528295aaf30d094ff
Kathrine94 <dhn@mail.ustc.edu.cn>
2024-02-29 14:30:01 +0800

    add glm result

---
+++ training/metax/glm-pytorch/config/config_C500x1x8.py
@@ -0,0 +1,18 @@
+train_batch_size = 16
+eval_batch_size = 16
+
+dist_backend = "nccl"
+
+lr = 1e-5
+weight_decay = 0.1
+adam_beta1 = 0.9
+adam_beta2 = 0.999
+adam_eps = 1e-08
+gradient_accumulation_steps = 1
+warmup = 0.1
+lr_decay_ratio = 0.1
+lr_decay_iters = 4338
+log_freq = 1
+seed = 10483
+max_samples_termination = 5553080
+training_event = None
