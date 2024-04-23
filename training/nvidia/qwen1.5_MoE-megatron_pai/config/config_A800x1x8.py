f7f985437b4a0985d3a15ed8d9ebb6490475a0fe
KerwinKai <kerwincheng@foxmail.com>
2024-04-23 23:11:39 +0800

    fix

--- training/nvidia/qwen1.5_MoE-megatron_pai/config/config_A800x1x8.py
+++ training/nvidia/qwen1.5_MoE-megatron_pai/config/config_A800x1x8.py
@@ -1,10 +1,10 @@
 batchsize = 1
-gbs = 32
-seqlength = 32768
-padlength = 32768
+gbs = 512
+seqlength = 8192
+padlength = 8192
 pr = 'bf16'
-tensor_parallel = 8
-pipeline_parallel = 1
+tensor_parallel = 1
+pipeline_parallel = 2
 accumulate_steps = 1
 theoryflops = 312000000000000.0
 epochs = 1
