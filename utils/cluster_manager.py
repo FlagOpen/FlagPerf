85d3c85779823f8a74cacedb86f10fecc3cbf1f8
KerwinKai <kerwincheng@foxmail.com>
2024-05-09 14:09:20 +0800

    Update cluster_manager.py

--- utils/cluster_manager.py
+++ utils/cluster_manager.py
@@ -89,7 +89,8 @@ class ClusterManager():
         if os.getenv("EXEC_IN_CONTAINER", False):
             if is_substring("../utils/image_manager.py", 
                                 ssh_run_cmd) or is_substring("../utils/sys_monitor.py",
-                                    ssh_run_cmd):
+                                ssh_run_cmd) or is_substring("../utils/nvidia_monitor.py",
+                                ssh_run_cmd):
                 ssh_run_cmd = replace_between_spaces(ssh_run_cmd, 3, 4, "python3")
         self.logger.debug("Run cmd on host with ssh. ssh cmd=" + ssh_run_cmd +
                           " host=" + host + " timeout=" + str(timeout))
