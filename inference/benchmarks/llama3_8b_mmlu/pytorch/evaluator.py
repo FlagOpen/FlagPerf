5e0b5b3e1b387b9e8c122fb2ebc8dd72eaf3fd9d
ljy-2000 <121491470+ljy-2000@users.noreply.github.com>
2024-05-10 14:25:28 +0800

    [BAAI] Add Llama3_8b_mmlu TorchInductor inference case (#528)
    * llama3-8b
    
    * fix
    
    * readme
    
    * upd readme
    
    * fix

---
+++ inference/benchmarks/llama3_8b_mmlu/pytorch/evaluator.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def evaluator(pred, y, dataloader):
+
+    tokenizer = dataloader.dataset.tokenizer
+
+    gt = y[0][0][0]
+    predict = pred[:, -1, :]
+    answer = torch.argmax(predict, dim=1)
+    answer_str = tokenizer.decode(answer)
+    valid_answers = ['A', 'B', 'C', 'D']
+    answer_str = ''.join([c for c in answer_str if c in valid_answers])
+    gt_str = tokenizer.decode(gt)
+    if answer_str == gt_str:
+        return 1
+    else:
+        return 0
