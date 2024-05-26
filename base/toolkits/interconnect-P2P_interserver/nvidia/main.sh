f607e44a2c6aac88b9faf78d1452527064153a6a
KerwinKai <kerwincheng@foxmail.com>
2024-05-26 21:06:43 +0800

    Update main.sh

--- base/toolkits/interconnect-P2P_interserver/nvidia/main.sh
+++ base/toolkits/interconnect-P2P_interserver/nvidia/main.sh
@@ -4,8 +4,8 @@ export NCCL_DEBUG=WARN
 nvcc -c -o bandwidth.o bandwidth.cu -I/usr/local/cuda/include -I/usr/local/nccl/include -I/usr/local/mpi/include
 mpic++ -o bdtest bandwidth.o -L/usr/local/cuda/lib64 -L/usr/local/nccl/lib -L/usr/local/mpi/lib -lcudart -lnccl -lcuda -lmpi
 echo "NODERANK: $NODERANK"
-sleep 36000
+sleep 5
 if [ "$NODERANK" -eq 0 ]; then
     echo "NODERANK is 0, executing the final command..."
-    mpirun --allow-run-as-root --host 10.1.2.155,10.1.2.158 -np 2 ./bdtest
+    mpirun --allow-run-as-root --host 10.1.2.155,10.1.2.158 -np 2 -x NCCL_DEBUG=WARN -x NCCL_IB_DISABLE=0 -x NCCL_IB_HCA=mlx5_2,mlx5_5 -x CUDA_DEVICE_MAX_CONNECTIONS=1 ./bdtest
 fi
