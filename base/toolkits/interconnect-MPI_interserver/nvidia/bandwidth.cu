f4f8f044077da05ffae1ee3d83d4d3f81b2cd9d3
KerwinKai <kerwincheng@foxmail.com>
2024-05-27 10:24:00 +0800

    update

--- base/toolkits/interconnect-MPI_interserver/nvidia/bandwidth.cu
+++ base/toolkits/interconnect-MPI_interserver/nvidia/bandwidth.cu
@@ -40,9 +40,9 @@ int main(int argc, char *argv[]) {
 
     printf("Rank: %d, Size: %d\n", rank, size);
 
-    int num_gpus_per_node = 8; // 每个节点的GPU数量
-    int total_gpus = num_gpus_per_node * size; // 总的GPU数量
-    int gpu_id = rank % num_gpus_per_node; // 当前进程使用的GPU ID
+    int num_gpus_per_node = 8;
+    int total_gpus = size;
+    int gpu_id = rank % num_gpus_per_node;
 
     cudaEvent_t start, end;
     float elapsed_time;
@@ -51,7 +51,6 @@ int main(int argc, char *argv[]) {
     ncclComm_t comm;
     cudaStream_t stream;
 
-    // 设置当前进程使用的GPU
     checkCudaError(cudaSetDevice(gpu_id), "cudaSetDevice");
     checkCudaError(cudaMalloc(&d_src, SIZE), "cudaMalloc");
     checkCudaError(cudaMalloc(&d_dst, SIZE), "cudaMalloc");
@@ -59,11 +58,7 @@ int main(int argc, char *argv[]) {
     checkCudaError(cudaStreamCreate(&stream), "cudaStreamCreate");
 
     ncclUniqueId id;
-    if (rank == 0){
-        printf("Generated NCCL unique id on rank 0 st\n");
-        checkNcclError(ncclGetUniqueId(&id), "ncclGetUniqueId");
-        printf("Generated NCCL unique id on rank 0 ed\n");
-    }
+    if (rank == 0) checkNcclError(ncclGetUniqueId(&id), "ncclGetUniqueId");
     checkMPIError(MPI_Bcast(&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD), "MPI_Bcast");
     printf("Before init nccl comm rank %d\n", rank);
 
@@ -78,23 +73,21 @@ int main(int argc, char *argv[]) {
     printf("start bandwidth test\n");
 
     for (int i = 0; i < WARMUP_ITERATIONS; ++i) {
-        checkNcclError(ncclGroupStart(), "ncclGroupStart");
         checkNcclError(ncclAllReduce((const void*)d_src, (void*)d_dst, SIZE / sizeof(float), ncclFloat, ncclSum, comm, stream), "ncclAllReduce");
-        checkNcclError(ncclGroupEnd(), "ncclGroupEnd");
         checkCudaError(cudaStreamSynchronize(stream), "cudaStreamSynchronize");
+        printf("warmup %d\n", i);
     }
-
+    checkMPIError(MPI_Barrier(MPI_COMM_WORLD), "MPI_Barrier");
     printf("warmup finished\n");
     checkCudaError(cudaEventRecord(start), "cudaEventRecord");
 
     for (int i = 0; i < ITERATIONS; ++i) {
-        checkNcclError(ncclGroupStart(), "ncclGroupStart");
         checkNcclError(ncclAllReduce((const void*)d_src, (void*)d_dst, SIZE / sizeof(float), ncclFloat, ncclSum, comm, stream), "ncclAllReduce");
-        checkNcclError(ncclGroupEnd(), "ncclGroupEnd");
         checkCudaError(cudaStreamSynchronize(stream), "cudaStreamSynchronize");
+        printf("iteration %d\n", i);
     }
     printf("iterations finished\n");
-
+    checkMPIError(MPI_Barrier(MPI_COMM_WORLD), "MPI_Barrier");
     checkCudaError(cudaEventRecord(end), "cudaEventRecord"); 
     checkCudaError(cudaEventSynchronize(end), "cudaEventSynchronize");
     checkCudaError(cudaEventElapsedTime(&elapsed_time, start, end), "cudaEventElapsedTime");
@@ -102,14 +95,10 @@ int main(int argc, char *argv[]) {
     double algbw = SIZE * ITERATIONS / (elapsed_time / 1000.0);
     double bandwidth = algbw * (2.0 * (total_gpus - 1) / total_gpus);
 
-    double global_bandwidth;
-    printf("MPI Reduce\n");
-    checkMPIError(MPI_Reduce(&bandwidth, &global_bandwidth, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD), "MPI_Reduce");
-    printf("MPI Reduce finished\n");
 
     if (rank == 0) {
-        printf("[FlagPerf Result]transfer-bandwidth=%.2fGiB/s\n", global_bandwidth / (1024.0 * 1024.0 * 1024.0));
-        printf("[FlagPerf Result]transfer-bandwidth=%.2fGB/s\n", global_bandwidth / (1000.0 * 1000.0 * 1000.0));
+        printf("[FlagPerf Result]transfer-bandwidth=%.2fGiB/s\n", bandwidth / (1024.0 * 1024.0 * 1024.0));
+        printf("[FlagPerf Result]transfer-bandwidth=%.2fGB/s\n", bandwidth / (1000.0 * 1000.0 * 1000.0));
     }
 
     checkCudaError(cudaFree(d_src), "cudaFree");
--- base/toolkits/interconnect-MPI_interserver/nvidia/main.sh
+++ base/toolkits/interconnect-MPI_interserver/nvidia/main.sh
@@ -1,8 +1,11 @@
 export NCCL_DEBUG=WARN
-export NCCL_IB_DISABLE=0;
-export NCCL_IB_CUDA_SUPPORT=1;
-export NCCL_IB_GID_INDEX=0;
-export NCCL_IB_HCA=mlx5_2,mlx5_8;
+export OPAL_PREFIX=/opt/hpcx/ompi
+export PATH=/usr/local/nvm/versions/node/v16.20.2/bin:/usr/local/lib/python3.10/dist-packages/torch_tensorrt/bin:/usr/local/mpi/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/ucx/bin:/opt/tensorrt/bin
+export LD_LIBRARY_PATH=/usr/local/lib/python3.10/dist-packages/torch/lib:/usr/local/lib/python3.10/dist-packages/torch_tensorrt/lib:/usr/local/cuda/compat/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64
 nvcc -c -o bandwidth.o bandwidth.cu -I/usr/local/cuda/include -I/usr/local/nccl/include -I/usr/local/mpi/include
 mpic++ -o bdtest bandwidth.o -L/usr/local/cuda/lib64 -L/usr/local/nccl/lib -L/usr/local/mpi/lib -lcudart -lnccl -lcuda -lmpi
-mpirun --allow-run-as-root -np 16 ./bdtest
+echo "NODERANK: $NODERANK"
+if [ "$NODERANK" -eq 0 ]; then
+    echo "NODERANK is 0, executing the final command..."
+    mpirun --allow-run-as-root --host 10.1.2.155:8,10.1.2.158:8 -np 16 -x NCCL_DEBUG=WARN -x NCCL_IB_DISABLE=0 -x NCCL_IB_HCA=mlx5_2,mlx5_5 -x CUDA_DEVICE_MAX_CONNECTIONS=1 ./bdtest
+fi
