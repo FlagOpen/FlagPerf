cd660c77b16ebceee12cfd838c8801ff38f84c98
shh2000 <13820618441@163.com>
2024-06-05 10:38:21 +0800

    init

---
+++ operation/benchmarks/mm/main.py
@@ -0,0 +1,119 @@
+# Copyright (c) 2024 BAAI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import torch
+import torch.distributed as dist
+import os
+import time
+from argparse import ArgumentParser, Namespace
+import yaml
+import sys
+sys.path.append("..")
+from drivers.utils import *
+
+
+def parse_args():
+    parser = ArgumentParser(description=" ")
+
+    parser.add_argument("--vendor",
+                        type=str,
+                        required=True,
+                        help="vendor name like nvidia")
+
+    parser.add_argument("--dataformat",
+                        type=str,
+                        required=True,
+                        help="like FP32,FP16")
+
+    parser.add_argument("--oplib",
+                        type=str,
+                        required=True,
+                        help="impl like pytorch/flaggems/cpp")
+
+    parser.add_argument("--chip",
+                        type=str,
+                        required=True,
+                        help="chip like A100_40_SXM")
+
+    args, unknown_args = parser.parse_known_args()
+    args.unknown_args = unknown_args
+    return args
+
+
+def main(config, case_config):    
+    print("Test Correctness with 16-times smaller operation")
+
+    m = case_config.M
+    n = case_config.N
+    k = case_config.K
+    
+    dtype = {"FP16": torch.float16}
+
+    mmape = []
+    
+    torch.manual_seed(42)
+    for i in range(100):
+        a = torch.randn((m//16, n//16), dtype=dtype[config.dataformat])
+        b = torch.randn((n//16, k//16), dtype=dtype[config.dataformat])
+
+        a_fp64 = a.to(torch.float64)
+        b_fp64 = b.to(torch.float64)
+        r_fp64 = torch.mm(a,b)
+
+        a = a.to(0)
+        b = b.to(0)
+
+        r_device = torch.mm(a, b).cpu()
+        mape = torch.mean(torch.abs(r_device - r_fp64) / torch.abs(r_fp64))
+        mmape.append(mape)
+    mape = torch.mean(torch.tensor(mmape))
+    
+    a = torch.randn((m, n), dtype=dtype[config.dataformat]).to(0)
+    b = torch.randn((n, k), dtype=dtype[config.dataformat]).to(0)
+
+    host_device_sync(config.vendor)
+    print("start warmup")
+    
+    for _ in range(case_config.WARMUP):
+        _tensor = torch.mm(a,b)
+
+
+    host_device_sync(config.vendor)
+    start_time = time.perf_counter()
+
+    for _ in range(case_config.ITERS):
+        _tensor = torch.mm(a,b)
+
+    host_device_sync(config.vendor)
+    end_time = time.perf_counter()
+
+    elapsed_time = end_time - start_time
+
+
+    datasize = case_config.ITERS * (m * n * k * 2)
+    tflops = datasize / elapsed_time / 1E12
+
+    return round(tflops, 2), mape
+
+
+if __name__ == "__main__":    
+    config = parse_args()
+    with open("case_config.yaml", "r") as file:
+        case_config = yaml.safe_load(file)
+    with open(os.path.join(config.vendor, config.chip,  "case_config.yaml"), "r") as file:
+        case_config_vendor = yaml.safe_load(file)
+    case_config.update(case_config_vendor)
+    case_config = Namespace(**case_config)
+
+    print(case_config)
+    if config.oplib == "flaggems":
+        import flag_gems
+        flag_gems.enable()
+        print("Using flaggems")
+    else:
+        print("Using nativetorch")
+    tflops, err = main(config, case_config)
+    print(r"[FlagPerf Result]{}'s computation-{}=".format(config.oplib, config.dataformat) + str(tflops) + "TFLOPS")
+    print(r"[FlagPerf Result]{}'s computation-{} mean relative error with FP64-CPU:{}".format(config.oplib, config.dataformat, err))
---
+++ operation/vendors/nvidia/ngctorch2403/ngctorch2403_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://mirror.ghproxy.com/https://github.com/FlagOpen/FlagGems.git
+cd FlagGems
+git checkout 982781081f5d62856064ae986e8927a31e96c235
+pip install .
