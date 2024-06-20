6dcbc0153bcb46f3f83fe1c4319e317a1281af87
tianxiao <tianxiao@baai.ac.cn>
2024-06-20 18:07:29 +0800

    Add generated code for add

--- operation/benchmarks/add/main.py
+++ operation/benchmarks/add/main.py
@@ -76,8 +76,8 @@ def main(config, case_config):
     mape = torch.mean(torch.tensor(mmape))
     mape_std = torch.std(torch.tensor(mmape))
 
-    a = torch.randn(m, dtype=dtype[config.dataformat]).to(0)
-    b = torch.randn(m, dtype=dtype[config.dataformat]).to(0)
+    a = torch.randn(m, 1024, 1024,  dtype=dtype[config.dataformat]).to(0)
+    b = torch.randn(m, 1024, 1024, dtype=dtype[config.dataformat]).to(0)
 
     latency_nowarm, latency_warm, cputime, kerneltime = do_test(
         torch.add, (a, b), host_device_sync, config, case_config)
