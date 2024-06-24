cd7be1f2df24526b80906b8ee969fd45e1e917e8
tianxiao <tianxiao@baai.ac.cn>
2024-06-24 16:32:41 +0800

    Add generated code for gt

--- operation/benchmarks/gt/main.py
+++ operation/benchmarks/gt/main.py
@@ -69,7 +69,7 @@ def main(config, case_config):
         a = a.to(0)
         b = b.to(0)
         r_device = torch.gt(a, b).cpu()
-        mape = torch.mean(torch.abs(r_device - r_fp64) / torch.abs(r_fp64))
+        mape = ((r_device != r_fp64).float().sum()/r_fp64.numel()).item()
 
         mmape.append(mape)
     
