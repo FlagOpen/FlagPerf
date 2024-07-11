9439d406042c68515b8d042d01e71e87003d307b
huiyiygy <huiyiygy@aliyun.com>
2024-07-11 15:35:38 +0800

    [metax] update retinanet config

--- training/metax/retinanet-pytorch/config/config_C500x2x8.py
+++ training/metax/retinanet-pytorch/config/config_C500x2x8.py
@@ -1,8 +1,8 @@
 vendor: str = "metax"
 
-train_batch_size = 16
-eval_batch_size = 16
-lr = 0.04
+train_batch_size = 22
+eval_batch_size = 22
+lr = 0.02
 max_epoch = 18
 seed = 101
 cudnn_benchmark = False
