710588621660d00ed1de6bdc6d7283a7ccd92291
tianxiao <tianxiao@baai.ac.cn>
2024-07-15 17:45:44 +0800

    Add generated code for isnan

--- operation/benchmarks/isnan/main.py
+++ operation/benchmarks/isnan/main.py
@@ -65,7 +65,7 @@ def main(config, case_config):
 
         a = a.to(0)
         r_device = torch.isnan(a).cpu()
-        mape = torch.mean(torch.where(r_fp64 == 0, torch.tensor(0.0), torch.abs(r_device - r_fp64) / torch.abs(r_fp64)))
+        mape = ((r_device != r_fp64).float().sum()/r_fp64.numel()).item()
 
         mmape.append(mape)
     
