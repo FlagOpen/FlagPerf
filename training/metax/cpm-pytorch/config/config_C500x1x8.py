859b19cc083df5f02820dbeb3241d5207e6c810e
Kathrine94 <dhn@mail.ustc.edu.cn>
2024-07-15 11:35:46 +0800

    modify

--- training/metax/cpm-pytorch/config/config_C500x1x8.py
+++ training/metax/cpm-pytorch/config/config_C500x1x8.py
@@ -7,13 +7,13 @@ target_embedding_average = 0.92
 
 gradient_accumulation_steps = 1
 
-train_batch_size = 192
+train_batch_size = 128
 eval_batch_size = train_batch_size
 max_steps = 4000
 max_samples_termination = 4391260
 
 warmup = 0.2
-learning_rate = 0.0005
+learning_rate = 0.002
 
 beta_1: float = 0.9
 beta_2: float = 0.99
