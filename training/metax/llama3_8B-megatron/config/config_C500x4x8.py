8a15b7d3549a76b626a4d36c9f7ad4415cad84fe
jsnoc <61768944+jsnoc@users.noreply.github.com>
2024-08-20 10:02:37 +0800

    add llama3-8b and llama2-70b in metax (#709)
    Co-authored-by: yaguang.wuyaguang <yaguang.wu@metax-tech.com>
    Co-authored-by: KerwinKai <kerwincheng@foxmail.com>

--- training/metax/llama3_8B-megatron/config/config_C500x4x8.py
+++ training/metax/llama3_8B-megatron/config/config_C500x4x8.py
@@ -1,7 +1,7 @@
 tokenizer_path = "llama3_8b_hf"
 localbs = 1
 train_steps = 300
-theoryflops = 312000000000000.0
+theoryflops = 280000000000000.0
 megatron_path = "/workspace/Megatron-LM_metax" # need to be aligned with DockerFile. In NGCtorch, it's /workspace/ + Megatron-LM
 tensor_parallel = 1
-pipeline_parallel = 4
+pipeline_parallel = 2
