b985f779ac3a1c3a08838b99807f908313427bd6
w4yne <w4yne.j@gmail.com>
2024-08-27 09:40:58 +0800

    [KUNLUNXIN] bugfix for op 'all' (#720)
    Co-authored-by: w4yne <zhouwei01@baidu.com>

--- operation/benchmarks/all/main.py
+++ operation/benchmarks/all/main.py
@@ -68,21 +68,21 @@ def main(config, case_config):
 
 
     m = case_config.Melements
-    # default shape: M * 1024 * 1024
-    shape = m * 1024 * 1024
+    # default arange: 0, M * 1024 * 1024
+    arange_end = m * 1024 * 1024
 
     if config.vendor == 'kunlunxin':
         # if `Shape' specified in `case_config.yaml', use it
         if case_config.__contains__('Shape') and case_config.Shape is not None:
-            shape = case_config.Shape
+            arange_end = math.prod(case_config.Shape)
 
-    a = torch.arange(0, math.prod(shape)).to(0)
+    a = torch.arange(0, arange_end).to(0)
     print(f'Shape for performance_test: {a.shape}')
 
     latency_nowarm, latency_warm, cputime, kerneltime = do_test(
         torch.all, (a, ), host_device_sync, config, case_config)
 
-    op2flops = lambda x: x * math.prod(shape)
+    op2flops = lambda x: x * arange_end
 
     perf_result = cal_perf(cputime, kerneltime, op2flops,
                            config.spectflops)
