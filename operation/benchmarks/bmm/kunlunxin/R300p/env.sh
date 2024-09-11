a843a8b3576fc7695f3394ff947ecf5f41be0ab4
w4yne <w4yne.j@gmail.com>
2024-09-11 09:13:21 +0800

    [KUNLUNXIN] add FlagGems blas op support & minor fix for env vars (#727)
    Co-authored-by: w4yne <zhouwei01@baidu.com>

--- operation/benchmarks/bmm/kunlunxin/R300p/env.sh
+++ operation/benchmarks/bmm/kunlunxin/R300p/env.sh
@@ -1,6 +1,5 @@
 echo "KUNLUNXIN ENV.SH start"
 
 source /root/miniconda/etc/profile.d/conda.sh && conda activate python38_torch201_cuda
-export TRITONXPU_BUFFER_SIZE=128
 
 echo "KUNLUNXIN ENV.SH end"
