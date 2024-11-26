304cc87912c0353ae8bd197d3a72f8370050025b
brianlcy123 <brianlcy123@gmail.com>
2024-11-26 17:50:29 +0800

    [kunlunxin] add llama3 70b patch

---
+++ training/kunlunxin/llama3_70B_continuetrain-flagscale/config/config_R300px4x8.py
@@ -0,0 +1,32 @@
+# scale_parent must under FlagPerf/ or data_dir/, otherwise you cannot mount it into baremetal, therefore cannot use shared storage
+scale_parent = "/share/project/PUBLIC/data/llama3-70b/FlagOpen"
+scale_home = f"{scale_parent}/FlagScale"
+
+# this cmd should install scale at <scale_home>. <scale_home> is set by flagperf.training.benchmarks.llava1.5_7b.flagscale.run_pretraining.py
+#scale_download_cmd = f"cd {scale_parent}; git clone https://github.com/FlagOpen/FlagScale.git; cd FlagScale; git checkout 085811f"
+scale_download_cmd = f"cd {scale_parent}; "
+
+# NV need nothing because all requirements have been established in base docker image. vendor can do anything related here
+scale_install_cmd = ""
+
+# locate energon. the copy from energon_install_path to flagscale/megatron/ is done by flagperf...run_pretraining.py
+energon_locate_cmd = r"pip show megatron-energon | grep Location | awk -F: '{print $2}' | xargs"
+
+scale_conf_dir = f"{scale_home}/examples/llama/conf"
+configyaml = f"{scale_conf_dir}/config.yaml"
+trainyaml = f"{scale_conf_dir}/train/train_llama3_70b_finetune.yaml"
+dataset = f"/share/project/PUBLIC/data/llama3-70b/llama3_dataset"
+tokenizer = f"/share/project/PUBLIC/data/llama3-70b/llama3_tokenizer"
+ckpt = f"/share/project/PUBLIC/data/llama3-70b/llama3_ckpt"
+
+
+cmds = {"before_start": "source ~/.bashrc; export LD_LIBRARY_PATH=/share/project/PUBLIC/data/llama3-70b/xpu_output//xccl/3.0.0.4_20241107/xccl_rdma-ubuntu_x86_64/so/:/share/project/PUBLIC/data/llama3-70b/xpu_output//xhpc/20241107/xhpc-ubuntu2004_x86_64/xblas/so:/share/project/PUBLIC/data/llama3-70b/xpu_output//xhpc/20241107/xhpc-ubuntu2004_x86_64/xfa/so:/share/project/PUBLIC/data/llama3-70b/xpu_output//xhpc/20241107/xhpc-ubuntu2004_x86_64/xpudnn/so:/share/project/PUBLIC/data/llama3-70b/xpu_output//xre/5.0.21.5/xre-Linux-x86_64-5.0.21.5/so"}
+
+
+# flagscale's requirements
+flagscale_chip_type = "R300p"
+flagscale_ssh_port = 3702
+flops = 999
+
+# for llava's algorithm
+steps = 500
