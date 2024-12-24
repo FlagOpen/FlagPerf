bec5e5894ac9c5b5f9cb00a9a4d4e345523a87fc
jiahuiwen-baai <hwjia@baai.ac.cn>
2024-12-24 15:24:21 +0800

    test

--- operation/benchmarks/ops/main.py
+++ operation/benchmarks/ops/main.py
@@ -61,43 +61,43 @@ def main(config):
     performance = do_performance()
     performance = performance == 0
 
-    # dtype = {
-    #     "FP32": torch.float32,
-    #     "FP16": torch.float16,
-    #     "BF16": torch.bfloat16,
-    #     "INT32": torch.int32,
-    #     "INT16": torch.int16,
-    #     "BOOL": torch.bool
-    #     }
-    # set_ieee_float32(config.vendor)
-    #
-    #
-    # m = case_config.Melements
-    #
-    #
-    # a = torch.randn(m, 1024, 1024, dtype=dtype[config.dataformat]).to(0)
-    #
-    # latency_nowarm, latency_warm, cputime, kerneltime = do_test(
-    #     torch.abs, (a, ), host_device_sync, config, case_config)
-    #
-    # op2flops = lambda x: x * m * 1024 * 1024
-    #
-    # perf_result = cal_perf(cputime, kerneltime, op2flops,
-    #                        config.spectflops)
-    # print_result(config, config.case_name, *perf_result, correctness,
-    #              latency_nowarm, latency_warm)
+    dtype = {
+        "FP32": torch.float32,
+        "FP16": torch.float16,
+        "BF16": torch.bfloat16,
+        "INT32": torch.int32,
+        "INT16": torch.int16,
+        "BOOL": torch.bool
+        }
+    set_ieee_float32(config.vendor)
+
+
+    m = case_config.Melements
+
+
+    a = torch.randn(m, 1024, 1024, dtype=dtype[config.dataformat]).to(0)
+
+    latency_nowarm, latency_warm, cputime, kerneltime = do_test(
+        torch.abs, (a, ), host_device_sync, config, case_config)
+
+    op2flops = lambda x: x * m * 1024 * 1024
+
+    perf_result = cal_perf(cputime, kerneltime, op2flops,
+                           config.spectflops)
+    print_result(config, config.case_name, *perf_result, correctness,
+                 latency_nowarm, latency_warm)
 
 
 if __name__ == "__main__":
     config = parse_args()
-    # with open("case_config.yaml", "r") as file:
-    #     case_config = yaml.safe_load(file)
-    # adapt_torch(config.vendor)
-    # with open(os.path.join(config.vendor, config.chip, "case_config.yaml"),
-    #           "r") as file:
-    #     case_config_vendor = yaml.safe_load(file)
-    # case_config.update(case_config_vendor)
-    # case_config = Namespace(**case_config)
+    with open("case_config.yaml", "r") as file:
+        case_config = yaml.safe_load(file)
+    adapt_torch(config.vendor)
+    with open(os.path.join(config.vendor, config.chip, "case_config.yaml"),
+              "r") as file:
+        case_config_vendor = yaml.safe_load(file)
+    case_config.update(case_config_vendor)
+    case_config = Namespace(**case_config)
 
     if config.oplib == "flaggems":
         import flag_gems
