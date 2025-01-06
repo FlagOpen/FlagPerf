e696a2c23ac0c839b79fd62bb13668392d1f281f
jiahuiwen-baai <hwjia@baai.ac.cn>
2025-01-06 15:22:36 +0800

    test

--- operation/benchmarks/drivers/calculate.py
+++ operation/benchmarks/drivers/calculate.py
@@ -29,7 +29,7 @@ def do_performance(mode, warmup):
     p = subprocess.Popen(
         # f"cd {os.path.join(gems_repo, 'benchmark')} && pytest --level core --mode {mode} --warmup {warmup} --record log ",
         # f"cd {os.path.join(gems_repo, 'benchmark')} && pytest -m mm --level core --mode {mode} --warmup {warmup} --record log -s",
-        f"cd {os.path.join(gems_repo, 'benchmark')} && pytest  test_generic_pointwise_perf.py --level  core --mode {mode} --warmup {warmup} --record log -s",
+        f"cd {os.path.join(gems_repo, 'benchmark')} && pytest  test_generic_pointwise_perf.py --level  core --mode {mode} --warmup {warmup} --record log",
         shell=True
     )
     p.wait()
