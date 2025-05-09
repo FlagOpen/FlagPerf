82ecbb2cf668aa864fdd84d633fd9b5be056d240
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-09 17:34:03 +0800

    Update main.sh

--- base/toolkits/interconnect-P2P_intraserver/ascend/A3/main.sh
+++ base/toolkits/interconnect-P2P_intraserver/ascend/A3/main.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:$LD_LIBRARY_PATH
 LOG_PATH=`pwd`
 ascend-dmi --bw -t p2p -m card -q > ${LOG_PATH}/test_result.log 2>&1
 RESULT=$(awk 'NR>21 && NR<23 {print $2}' ${LOG_PATH}/test_result.log)
-echo "[FlagPref Result] interconnect-P2P_intraserver-bandwidth=${RESULT} GB/s"
+echo "[FlagPerf Result] interconnect-P2P_intraserver-bandwidth=${RESULT} GB/s"
 rm -rf ${LOG_PATH}/test_result.log
