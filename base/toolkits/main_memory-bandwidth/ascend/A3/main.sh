f098310a773b331338c2a1df03429926e56c8699
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-09 19:21:41 +0800

    Update main.sh

--- base/toolkits/main_memory-bandwidth/ascend/A3/main.sh
+++ base/toolkits/main_memory-bandwidth/ascend/A3/main.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:$LD_LIBRARY_PATH
 LOG_PATH=`pwd`
 ascend-dmi --bw -t d2d -q > ${LOG_PATH}/test_result.log 2>&1
 RESULT=$(awk 'NR>29 && NR<31 {print $3}' ${LOG_PATH}/test_result.log)
-RESULT_INT=$(print "%.0f" "$RESULT")
+RESULT_INT=$(printf "%.0f" "$RESULT")
 RESULT_A3=$(expr $RESULT_INT \* 2)
 echo "[FlagPerf Result] main_memory-bandwidth=${RESULT_A3} GB/s"
 rm -rf ${LOG_PATH}/test_result.log
