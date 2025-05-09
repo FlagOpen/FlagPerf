420bf34745d5cc0e105634c614292667d1273462
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-09 17:31:33 +0800

    Update main.sh

--- base/toolkits/computation-FP16/ascend/A3/main.sh
+++ base/toolkits/computation-FP16/ascend/A3/main.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:$LD_LIBRARY_PATH
 LOG_PATH=`pwd`
 ascend-dmi -f -t fp16 -q > ${LOG_PATH}/test_result.log 2>&1
 RESULT=$(awk 'NR>3 && NR<5 {print $4}' ${LOG_PATH}/test_result.log)
-echo "[FlagPref Result] computation-FP16=$RESULT TFLOPS"
+echo "[FlagPerf Result] computation-FP16=$RESULT TFLOPS"
 rm -rf ${LOG_PATH}/test_result.log
