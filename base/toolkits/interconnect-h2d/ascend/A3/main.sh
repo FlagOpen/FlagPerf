7e2530a119105b187c9a187b56f37d2fdef04890
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-10 19:57:11 +0800

    Update main.sh

--- base/toolkits/interconnect-h2d/ascend/A3/main.sh
+++ base/toolkits/interconnect-h2d/ascend/A3/main.sh
@@ -2,7 +2,7 @@ source /usr/local/Ascend/toolbox/set_env.sh
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:$LD_LIBRARY_PATH
 LOG_PATH=`pwd`
-ascend-dmi --bw -t h2d -s 4294967296 --et 100 -q > ${LOG_PATH}/test_result.log 2>&1
+ascend-dmi --bw -t h2d -s 536870912 --et 50 -q > ${LOG_PATH}/test_result.log 2>&1
 RESULT=$(awk 'NR>5 && NR<7 {print $4}' ${LOG_PATH}/test_result.log)
 echo "[FlagPerf Result] interconnect-h2d bandwidth=$RESULT GB/s"
 rm -rf ${LOG_PATH}/test_result.log
