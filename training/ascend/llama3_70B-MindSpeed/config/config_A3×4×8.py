6ffdeb69e9ee3e23538030dc8374facece23221b
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-12 10:19:44 +0800

    Delete training/ascend/llama3_70B-MindSpeed directory

--- training/ascend/llama3_70B-MindSpeed/config/config_A3×4×8.py
+++
@@ -1,7 +0,0 @@
-tokenizer_path = "/home/zhiyuan/tokenizer_a3"
-localbs = 1
-train_steps = 30
-theoryflops = 752000000000000.0
-megatron_path = "/workspace/Megatron-LM"
-tensor_parallel = 8
-pipeline_parallel = 4
