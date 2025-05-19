c9ad18bac64dcf41815bb1eb47745d59a09f8f35
zhangyq6 <zhangyuqi@MacBook-Pro-2.local>
2025-05-19 21:15:04 +0800

    update: fix scripts

--- base/toolkits/interconnect-MPI_intraserver/dcu/BW/main.sh
+++ base/toolkits/interconnect-MPI_intraserver/dcu/BW/main.sh
@@ -1,6 +1,7 @@
 LOG_PATH=`pwd`/`hostname -i | awk '{print $1}'`_run_log
-mpirun -x NCCL_GRAPH_FILE=/opt/single_graph.xml -x NCCL_P2P_LEVEL=SYS -x NCCL_MIN_NCHANNELS=32 -x NCCL_MIN_P2P_NCHANNELS=32 -x NCCL_NCHANNELS_PER_PEER=32 --allow-run-as-root -np 8 sendrecv_perf -b 256M -e 256M -f 2 -g 1 2>&1 | tee ${LOG_PATH}
+mpirun -x NCCL_GRAPH_FILE=/opt/single_graph.xml -x NCCL_P2P_LEVEL=SYS -x NCCL_MIN_NCHANNELS=32 -x NCCL_MIN_P2P_NCHANNELS=32 --allow-run-as-root -np 8 all_reduce_perf -b 256M -e 256M -f 2 -g 1 2>&1 | tee ${LOG_PATH}
+
 data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
-# result=$(python3 -c "print(float($data) * 2)")
+
 echo "[FlagPerf Result]interconnect-MPI_intraserver-bandwidth=$data GB/s"
 rm -rf ${LOG_PATH}
--- base/toolkits/interconnect-P2P_interserver/dcu/BW/main.sh
+++ base/toolkits/interconnect-P2P_interserver/dcu/BW/main.sh
@@ -4,7 +4,6 @@ IFS=',' read -ra ADDR <<< "$hosts"
 ip1=$(echo "${ADDR[0]}" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/"//g')
 ip2=$(echo "${ADDR[1]}" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/"//g')
 
-# step-1 配置免密
 echo 'root:123456' | sudo chpasswd
 rm -rf ~/.ssh/* && ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa -q
 sed -i '/StrictHostKeyChecking/c StrictHostKeyChecking no' /etc/ssh/ssh_config
@@ -16,16 +15,32 @@ sleep 10
 sshpass -p "123456" ssh-copy-id -i ~/.ssh/id_rsa.pub -p 1234 root@${ip1}
 sshpass -p "123456" ssh-copy-id -i ~/.ssh/id_rsa.pub -p 1234 root@${ip2}
 
-LOG_PATH=`pwd`/`hostname -i | awk '{print $1}'`_run_log
+apt-get install -y iproute2
+LOG_PATH=$(pwd)/$(ip a | grep -w 'inet' | grep 'global' | sed 's/.*inet //;s/\/.*//' | awk 'NR==1{print $1}')_run_log
 
-mpirun -x NCCL_TOPO_FILE=/opt/topo.xml  -x  NCCL_IB_HCA=mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_8,mlx5_9,mlx5_10,mlx5_11 \
-    --mca btl_tcp_if_include ${ip1}/24 \
-    --allow-run-as-root \
-    --host ${ip1}:1,${ip2}:1 \
-    -mca plm_rsh_args "-p 1234" -np 2 -x LD_LIBRARY_PATH=/opt/hyhal/lib:${LD_LIBRARY_PATH} \
-    -x UCX_NET_DEVICES=mlx5_6:1 -x NCCL_NET_GDR_LEVEL=5 -x NCCL_NET_GDR_READ=1 sendrecv_perf -b 1g -e 1g -g 1 -f 2 2>&1 | tee ${LOG_PATH}
 
-data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
-# result=$(python3 -c "print(float($data) * 2)")
-echo "[FlagPerf Result]interconnect-P2P_interserver-bandwidth=$data GB/s"
-rm -rf ${LOG_PATH}
+cur_ip=$(ip a | grep -w 'inet' | grep 'global' | sed 's/.*inet //;s/\/.*//' | awk 'NR==1{print $1}')
+if [[ "${cur_ip}" == "${ip1}" ]];then
+        mpirun -x NCCL_TOPO_FILE=/opt/topo.xml \
+                --mca btl_tcp_if_include ${ip1}/24 --allow-run-as-root --host ${ip1}:1,${ip2}:1 \
+                -mca plm_rsh_args "-p 1234" -np 2 -x LD_LIBRARY_PATH -x UCX_NET_DEVICES=mlx5_6:1 \
+                -x NCCL_NET_GDR_LEVEL=5 -x NCCL_NET_GDR_READ=1 -x NCCL_IB_HCA=mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_8,mlx5_9,mlx5_10,mlx5_11 sendrecv_perf -b 1g -e 1g -g 1 -f 2  2>&1 | tee ${LOG_PATH}
+        data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
+        while  [ ! -f ${ip2}_run_log ] || ! grep -q "Avg bus bandwidth" ${ip2}_run_log ; do
+                sleep 1 
+        done
+        echo "[FlagPerf Result]interconnect-P2P_interserver-bandwidth=$data GB/s"
+        rm -rf ${ip1}_run_log ${ip2}_run_log
+else
+        while [ ! -f ${ip1}_run_log ] || ! grep -q "Avg bus bandwidth" ${ip1}_run_log ; do
+                sleep 1 
+        done
+        mpirun -x NCCL_TOPO_FILE=/opt/topo.xml \
+                --mca btl_tcp_if_include ${ip1}/24 --allow-run-as-root --host ${ip1}:1,${ip2}:1 \
+                -mca plm_rsh_args "-p 1234" -np 2 -x LD_LIBRARY_PATH -x UCX_NET_DEVICES=mlx5_6:1 \
+                -x NCCL_NET_GDR_LEVEL=5 -x NCCL_NET_GDR_READ=1 -x NCCL_IB_HCA=mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_8,mlx5_9,mlx5_10,mlx5_11 sendrecv_perf -b 1g -e 1g -g 1 -f 2  2>&1 | tee ${LOG_PATH}
+
+        data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
+        echo "[FlagPerf Result]interconnect-P2P_interserver-bandwidth=$data GB/s"
+
+fi
