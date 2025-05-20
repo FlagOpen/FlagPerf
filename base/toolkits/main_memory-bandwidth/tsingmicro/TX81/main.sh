5ede0fbf5a3c9cb4d12055832f539ea4f3b9b129
alvin-zzw <zhangzhenwen@tsingmicro.com>
2025-05-20 13:58:51 +0800

    Tsingmicro (#778)
    * [tsingmicro]add dockerfile
    
    * [tsingmicro] add tsingmicro test case
    
    * [tsingmicro] add test case bin data
    
    * [tsingmicro] update toolkits bin files
    
    * [tsingmicro] update test case version
    
    * [tsingmicro] fix c2c latency result parse
    
    * [tsingmicro] reduce file size

---
+++ base/toolkits/main_memory-bandwidth/tsingmicro/TX81/main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curr_path=$(pwd)
+
+vendor_path=../../../../vendors/tsingmicro
+exec_path=/root/TX81
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$exec_path
+
+cd $exec_path
+./tsmPerf -r lsuPerf -d chip_out/ -f 0 | tee $curr_path/test_result.log
+cd -
+
+python3 $vendor_path/log_analysis.py --log_type="lsu_test" --log_file="./test_result.log"
+rm -f ./test_result.log
+
