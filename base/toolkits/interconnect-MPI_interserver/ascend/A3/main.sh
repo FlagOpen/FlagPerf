4b5035679a7adb199c0356032a625a4b2a834600
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-21 12:45:02 +0800

    [Huawei] Code change for MPI test (#800)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete base/toolkits/interconnect-P2P_intraserver/ascend directory
    
    * Add files via upload
    
    * Delete base/toolkits/interconnect-h2d/ascend directory
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update ascend_analysis.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update ascend_monitor.py
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update host.yaml
    
    * Delete training/ascend/llama3_70B-MindSpeed directory
    
    * Delete training/benchmarks/llama3_70B/MindSpeed directory
    
    * Delete training/run_benchmarks/MindSpeed directory
    
    * Create Dockerfile
    
    * Create pytorch_2.1_install.sh
    
    * Update host.yaml
    
    * Update run.py
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update host.yaml
    
    * Update run.py
    
    * Update host.yaml
    
    * Update main.sh
    
    * Update main.sh

--- base/toolkits/interconnect-MPI_interserver/ascend/A3/main.sh
+++ base/toolkits/interconnect-MPI_interserver/ascend/A3/main.sh
@@ -4,10 +4,11 @@ export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:$LD_LIBRARY_PATH
 export INSTALL_DIR=/usr/local/Ascend/ascend-toolkit/latest
 export PATH=/usr/local/mpich/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/mpich/lib:${INSTALL_DIR}/lib64:$LD_LIBRARY_PATH
-export HCCL_BUFFERSIZE=4096
+export HCCL_BUFFSIZE=4096
 LOG_PATH=`pwd`
 cd /usr/local/Ascend/ascend-toolkit/latest/tools/hccl_test
-mpirun -f hostfile -n 32 ./bin/alltoall_test -b 512 -e 4G -f 2 -p 16 > ${LOG_PATH}/test_result.log 2>&1
+mpirun -f hostfile -n 32 ./bin/all_gather_test -b 512 -e 4G -f 2 -p 16 > ${LOG_PATH}/test_result.log 2>&1
+#mpirun -f hostfile -n 32 ./bin/alltoall_test -b 512 -e 4G -f 2 -p 16 > ${LOG_PATH}/test_result.log 2>&1
 #mpirun -f hostfile -n 32 ./bin/all_reduce_test -b 512 -e 4G -f 2 -p 16 > ${LOG_PATH}/test_result.log 2>&1
 #mpirun -f hostfile -n 32 ./bin/reduce_scatter_test -b 512 -e 4G -f 2 -p 16 > ${LOG_PATH}/test_result.log 2>&1
 RESULT=$(awk 'END{print $5}' ${LOG_PATH}/test_result.log)
