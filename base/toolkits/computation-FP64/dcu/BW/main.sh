2bc9a77e72e3ab1031565a9cf7d871bd63582eea
YuQ-sudo <55684266+YuQ-sudo@users.noreply.github.com>
2025-05-26 11:23:30 +0800

    [hygon]:update benchmark tools (#801)
    * feat:add dcu base test
    
    * fix:parameters
    
    * fix: fix log
    
    * fix: interserver params tune
    
    * fix: tune params
    
    * fix:reverse configs yaml
    
    * fix:remove int8
    
    * fix:reverse params
    
    * fix:remove useless readme.md
    
    * fix:remove redundancy files
    
    * [hygon]:fix memory capacity scripts
    
    * update: fix scripts
    
    * update:using hyqual benchmark tool
    
    * fix:remove useless files
    
    ---------
    
    Co-authored-by: zhangyq6 <zhangyuqi@MacBook-Pro-2.local>
    Co-authored-by: zhangyq6 <zhangyuqi@MacBook-Pro-2.lan>

--- base/toolkits/computation-FP64/dcu/BW/main.sh
+++ base/toolkits/computation-FP64/dcu/BW/main.sh
@@ -1,2 +1,8 @@
-hipcc -O3 gemm.cu -lhipblas -o gemm -std=c++17 --offload-arch=gfx936
-./gemm
+LOG_PATH=$(pwd)/$(ip a | grep -w 'inet' | grep 'global' | sed 's/.*inet //;s/\/.*//' | awk 'NR==1{print $1}')_run_log
+
+export PATH=/opt/hyqual_v3.0.3:${PATH}
+run 7 2>&1 | tee ${LOG_PATH}
+
+data=$(grep 'peak dgemm    :'  ${LOG_PATH} | awk '{print $4}' | sort -nr | head -n1)
+echo "[FlagPerf Result]computation-FP64=$data TFLOPS"
+rm -rf ${LOG_PATH}
