4e177bce51f350beca02d1be32287d0fed7db958
zhangyq6 <zhangyuqi@MacBook-Pro-2.local>
2025-06-06 16:59:23 +0800

    fix:align params

--- base/toolkits/interconnect-P2P_intraserver/dcu/BW/main.sh
+++ base/toolkits/interconnect-P2P_intraserver/dcu/BW/main.sh
@@ -1,4 +1,5 @@
-LOG_PATH=`pwd`/`hostname -i | awk '{print $1}'`_run_log
+LOG_PATH=$(pwd)/$(ip a | grep -w 'inet' | grep 'global' | sed 's/.*inet //;s/\/.*//' | awk 'NR==1{print $1}')_run_log
+
 mpirun -x NCCL_GRAPH_FILE=/opt/single_graph.xml -x NCCL_P2P_LEVEL=SYS -x NCCL_MIN_NCHANNELS=32 -x NCCL_MIN_P2P_NCHANNELS=32 -x NCCL_NCHANNELS_PER_PEER=32 -x RCCL_SDMA_COUNT_ENABLE=1 -x RCCL_SDMA_COPY_ENABLE=1 -x RCCL_COLL_XHCL_CHANNEL_NUM=28 --allow-run-as-root -np 8 sendrecv_perf -b 1g -e 1g -f 2 -g 1 2>&1 | tee ${LOG_PATH}
 data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
 
