9859883a7506b54aae3c05a4582b7f60c0cc22f2
zhangyq6 <zhangyuqi@MacBook-Pro-2.lan>
2025-06-06 18:59:33 +0800

    update:align params

--- base/toolkits/interconnect-MPI_intraserver/dcu/BW/main.sh
+++ base/toolkits/interconnect-MPI_intraserver/dcu/BW/main.sh
@@ -3,6 +3,6 @@ LOG_PATH=$(pwd)/$(ip a | grep -w 'inet' | grep 'global' | sed 's/.*inet //;s/\/.
 mpirun -x NCCL_GRAPH_FILE=/opt/single_graph.xml -x NCCL_P2P_LEVEL=SYS -x NCCL_MIN_NCHANNELS=32 -x NCCL_MIN_P2P_NCHANNELS=32 -x RCCL_SDMA_COUNT_ENABLE=1 -x RCCL_SDMA_COPY_ENABLE=1 -x RCCL_COLL_XHCL_CHANNEL_NUM=28 --allow-run-as-root -np 8 all_reduce_perf -b 1g -e 1g -f 2 -g 1 2>&1 | tee ${LOG_PATH}
 
 data=$(grep "# Avg bus bandwidth" ${LOG_PATH} | awk '{print $NF}')
-
-echo "[FlagPerf Result]interconnect-MPI_intraserver-bandwidth=$data GB/s"
+result=$(python3 -c "print(float($data) * 2)")
+echo "[FlagPerf Result]interconnect-MPI_intraserver-bandwidth=$result GB/s"
 rm -rf ${LOG_PATH}
