2582308e6c1673c1f8ede05acaf5d0e1f34875c2
Fanglai Peng <fanglai.peng@iluvatar.ai>
2025-06-11 17:59:04 +0800

    [iluvatar] update base test env.sh

--- base/benchmarks/interconnect-P2P_intraserver/iluvatar/BI150/env.sh
+++ base/benchmarks/interconnect-P2P_intraserver/iluvatar/BI150/env.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=/usr/local/corex/lib64/python3/dist-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=/usr/local/corex/lib64:$LD_LIBRARY_PATH
 export PATH=/usr/local/corex/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/corex/lib64/python3/dist-packages/bin:$PATH
+export NCCL_SHM_DISABLE=1
